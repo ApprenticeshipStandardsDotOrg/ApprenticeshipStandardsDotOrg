25003e478a68a27ee7192c73b0bd15a1447509e9
Jeanine Soterwood <jeanine@thoughtbot.com>
2022-11-15 09:13:24 -0800

    Stub new form for standards import and add system spec

---
+++ spec/system/standards_imports/new_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe "standards_imports/new" do
+  it "create a new standards_imports record" do
+    visit new_standards_import_path
+    fill_in "Name", with: "Mickey Mouse"
+    fill_in "Email", with: "mickey@example.com"
+    fill_in "Organization", with: "Disney"
+    fill_in "Notes", with: "a"*500
+
+    expect{
+      click_on "Create Standards import"
+    }.to change(StandardsImport, :count).by(1)
+
+    si = StandardsImport.last
+    expect(si.name).to eq "Mickey Mouse"
+    expect(si.email).to eq "mickey@mouse.com"
+    expect(si.organization).to eq "Disney"
+    expect(si.notes).to eq "a"*500
+
+    expect(page).to have_text "Mickey Mouse"
+    expect(page).to have_text "mickey@mouse.com"
+    expect(page).to have_text "Disney"
+    expect(page).to have_text "a"*500
+  end
+end
