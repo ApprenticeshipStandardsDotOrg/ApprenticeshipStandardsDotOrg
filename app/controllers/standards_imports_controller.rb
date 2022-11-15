6b93775c2dd56100d1dc6e9d7527fde84559cb35
Jeanine Soterwood <jeanine@thoughtbot.com>
2022-11-15 09:10:09 -0800

    Add create method for standards_import controller

--- app/controllers/standards_imports_controller.rb
+++ app/controllers/standards_imports_controller.rb
@@ -1,10 +1,24 @@
 class StandardsImportsController < ApplicationController
   def new
+    @standards_import = StandardsImport.new
   end
 
   def create
+    @standards_import = StandardsImport.new(create_params)
+
+    if @standards_import.save
+      redirect_to standards_import_path(@standards_import)
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  def create_params
+    params.require(:standards_import).permit(:name, :email, :organization, :notes)
   end
 end
--- spec/requests/standards_imports_spec.rb
+++ spec/requests/standards_imports_spec.rb
@@ -8,4 +8,47 @@ RSpec.describe "StandardsImports", type: :request do
       expect(response).to be_successful
     end
   end
+
+  describe "POST /create" do
+    context "with valid parameters" do
+      it "creates new standards import record and redirects to show page" do
+        expect {
+          post standards_imports_path, params: {
+            standards_import: {
+              name: "Mickey Mouse",
+              email: "mickey@mouse.com",
+              organization: "Disney",
+              notes: "a"*500
+            }
+          }
+        }.to change(StandardsImport, :count).by(1)
+
+        si = StandardsImport.last
+        expect(si.name).to eq "Mickey Mouse"
+        expect(si.email).to eq "mickey@mouse.com"
+        expect(si.organization).to eq "Disney"
+        expect(si.notes).to eq "a"*500
+
+        expect(response).to redirect_to standards_import_path(si)
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not create new standards import record and renders new" do
+        allow_any_instance_of(StandardsImport).to receive(:save).and_return(false)
+        expect {
+          post standards_imports_path, params: {
+            standards_import: {
+              name: "Mickey Mouse",
+              email: "mickey@mouse.com",
+              organization: "Disney",
+              notes: "a"*500
+            }
+          }
+        }.to_not change(StandardsImport, :count)
+
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
 end
