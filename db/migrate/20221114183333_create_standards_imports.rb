e03cd386281159458a0d6bae41ffe6aa3815b224
Jeanine Soterwood <jeanine@thoughtbot.com>
2022-11-17 09:38:37 -0800

    Use UUIDs for all primary keys

--- db/migrate/20221114183333_create_standards_imports.rb
+++ db/migrate/20221114183333_create_standards_imports.rb
@@ -1,6 +1,6 @@
 class CreateStandardsImports < ActiveRecord::Migration[7.0]
   def change
-    create_table :standards_imports do |t|
+    create_table :standards_imports, id: :uuid do |t|
       t.string :name
       t.string :email
       t.string :organization
