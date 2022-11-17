2a508143236c508175dbe5db0435839c12aa3961
Jeanine Soterwood <jeanine@thoughtbot.com>
2022-11-17 09:31:57 -0800

    Enable pgcrypto extension so we can use UUIDs as primary keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_14_183333) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_173104) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
