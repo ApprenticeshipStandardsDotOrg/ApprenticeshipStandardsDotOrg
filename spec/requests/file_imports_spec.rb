f95219cf9a0cc3da6852ac8b7ecae312a863564c
Jeanine Soterwood <jeanine@thoughtbot.com>
2022-11-17 12:49:52 -0800

    Move ImportFiles to FileImports

--- spec/requests/file_imports_spec.rb
+++ spec/requests/file_imports_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
-RSpec.describe "StandardsImports", type: :request do
+RSpec.describe "FileImports", type: :request do
   describe "GET /index" do
     context "when admin user" do
       it "returns http success" do
         admin = create(:admin)
 
         sign_in admin
-        get import_files_path
+        get file_imports_path
 
         expect(response).to be_successful
       end
@@ -15,7 +15,7 @@ RSpec.describe "StandardsImports", type: :request do
 
     context "when guest" do
       it "redirects to root path" do
-        get import_files_path
+        get file_imports_path
 
         expect(response).to redirect_to new_user_session_path
       end
