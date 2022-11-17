a790fc413afd17f23cb7dc0484b2716c6ca2d06b
Jeanine Soterwood <jeanine@thoughtbot.com>
2022-11-17 12:09:47 -0800

    Define roles and add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+if Rails.env.development?
+  User.where(email: "admin@example.com").first_or_create!(
+    role: "admin",
+    name: "Admin User",
+    password: "passwordpassword"
+  )
+end
