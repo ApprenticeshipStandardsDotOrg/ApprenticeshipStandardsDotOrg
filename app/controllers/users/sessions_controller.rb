fe8ac3bbc976c179b84aa6b5e574d9785f95c2e4
Jeanine Soterwood <jeanine@thoughtbot.com>
2022-11-21 13:09:23 -0800

    Reinstate link_to with delete method for Devise sign out. The redirect needs to be 303 for Turbo. https://github.com/heartcombo/devise/issues/5458#issuecomment-1235991534

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  def destroy
+    super do
+      # Turbo requires redirects be :see_other (303); so override Devise default (302)
+      return redirect_to after_sign_out_path_for(resource_name), status: :see_other
+    end
+  end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
