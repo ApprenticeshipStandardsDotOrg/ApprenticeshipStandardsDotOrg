a1e15ac5bd7ca3d31da23c9988f867e535e87c09
Jeanine Soterwood <jeanine@thoughtbot.com>
2022-11-21 13:30:51 -0800

    Remove user sign up routes
    https://stackoverflow.com/a/39780131/1753903

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: "users/sessions"
-  }
+  devise_for :users,
+    skip: :registrations,
+    controllers: {
+      sessions: "users/sessions"
+    }
+    as :user do
+      get "users/edit", to: "devise/registrations#edit", as: "edit_user_registration"
+      patch "users", to: "devise/registrations#update", as: "user_registration"
+      put "users", to: "devise/registrations#update", as: nil
+    end
 
   root to: "standards_imports#new"
   resources :standards_imports, only: [:new, :create, :show]
