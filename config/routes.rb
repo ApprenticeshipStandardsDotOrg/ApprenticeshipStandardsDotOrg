61f18042fd662702c0ba09325b2f2e29a25df59f
Jeanine Soterwood <jeanine@thoughtbot.com>
2022-11-21 13:09:23 -0800

    Reinstate link_to with delete method for Devise sign out. The redirect needs to be 303 for Turbo. https://github.com/heartcombo/devise/issues/5458#issuecomment-1235991534

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, controllers: {
+    sessions: "users/sessions"
+  }
 
   root to: "standards_imports#new"
   resources :standards_imports, only: [:new, :create, :show]
