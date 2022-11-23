1bb24383011852fdaaee28bd7da025b401c60e85
Jeanine Soterwood <jeanine@thoughtbot.com>
2022-11-23 13:10:01 -0800

    Add admin.example.localhost to dev hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,6 +70,8 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  config.hosts << "admin.example.localhost"
+
   config.after_initialize do
     Bullet.enable = true
     Bullet.alert = true
