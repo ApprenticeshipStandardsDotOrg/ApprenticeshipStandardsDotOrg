d7d0efb901273b2c2bfcda4531386800c99861eb
Jeanine Soterwood <jeanine@thoughtbot.com>
2022-11-28 15:42:49 -0800

    Minor: add line breaks for spec consistency

--- spec/models/active_storage_attachment_spec.rb
+++ spec/models/active_storage_attachment_spec.rb
@@ -3,13 +3,16 @@ require "rails_helper"
 RSpec.describe ActiveStorage::Attachment, type: :model do
   it "has a valid factory" do
     attachment = build(:active_storage_attachment)
+
     expect(attachment).to be_valid
   end
 
   it "creates a file import record when type is standards import" do
     import = create(:standards_import)
     attachment = build(:active_storage_attachment, record: import)
+
     expect { attachment.save! }.to change(FileImport, :count).by(1)
+
     file_import = FileImport.last
     expect(file_import.active_storage_attachment).to eq attachment
   end
@@ -17,6 +20,7 @@ RSpec.describe ActiveStorage::Attachment, type: :model do
   it "does not create a file import record when type is not standards import" do
     user = create(:user)
     attachment = build(:active_storage_attachment, record: user)
+
     expect { attachment.save! }.to_not change(FileImport, :count)
   end
 end
--- spec/system/file_imports/index_spec.rb
+++ spec/system/file_imports/index_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe "file_imports/index" do
   it "displays status and link to file" do
     create :standards_import, :with_files
     admin = create :admin
+
     login_as admin
     visit file_imports_path
+
     expect(page).to have_content("Pending").once
     expect(page).to have_link("pixel1x1.jpg")
   end
