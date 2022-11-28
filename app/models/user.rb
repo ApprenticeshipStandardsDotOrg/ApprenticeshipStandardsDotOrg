16ab9ac87d138715450562036fb4dd877ad1bf7b
Jeanine Soterwood <jeanine@thoughtbot.com>
2022-11-28 15:22:54 -0800

    lint

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :trackable
+    :recoverable, :rememberable, :validatable,
+    :trackable
 
   enum :role, [:basic, :admin]
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) {|n| "user#{n}@example.com"}
+    sequence(:email) { |n| "user#{n}@example.com" }
     password { "passwordpassword" }
 
     factory :admin do
