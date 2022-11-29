f05454c5be1270aafdc6ac65d15e7ecd1ced2677
Jeanine Soterwood <jeanine@thoughtbot.com>
2022-11-28 16:07:48 -0800

    Add link to file on file_imports#index page

--- app/controllers/file_imports_controller.rb
+++ app/controllers/file_imports_controller.rb
@@ -1,5 +1,6 @@
 class FileImportsController < ApplicationController
   before_action :authenticate_user!, only: [:index]
+  include ActiveStorage::SetCurrent
 
   def index
     @file_imports = FileImport.includes(active_storage_attachment: :blob)
