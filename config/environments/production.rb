45b0416ff32f9e05bc553130353033112a6286a3
Jeanine Soterwood <jeanine@thoughtbot.com>
2022-12-20 09:47:48 -0800

    Add default_url_options host for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "ApprenticeshipStandardsDotOrg_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {host: ENV["APP_DOMAIN"]}
 
   ActionMailer::Base.smtp_settings = {
     port: ENV["MAILGUN_SMTP_PORT"],
