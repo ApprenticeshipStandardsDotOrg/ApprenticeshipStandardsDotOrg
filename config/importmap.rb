53158290d400701d14593770d829c4b52ca15ea9
Jennifer Lu <58493319+jenlu33@users.noreply.github.com>
2023-01-03 11:58:31 -0800

    Set up google analytics (#22)

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,5 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "google_analytics"
 pin_all_from "app/javascript/controllers", under: "controllers"
