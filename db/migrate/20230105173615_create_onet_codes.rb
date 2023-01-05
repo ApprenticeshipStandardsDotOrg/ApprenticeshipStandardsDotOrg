bbfd96be99338ca99eb891848d7d8ae1340dce17
Jennifer Lu <nejul33@gmail.com>
2023-01-05 12:51:08 -0500

    Lint

--- db/migrate/20230105173615_create_onet_codes.rb
+++ db/migrate/20230105173615_create_onet_codes.rb
@@ -3,7 +3,7 @@ class CreateOnetCodes < ActiveRecord::Migration[7.0]
     create_table :onet_codes, id: :uuid do |t|
       t.string :name
       t.string :code
-      
+
       t.timestamps
     end
   end
