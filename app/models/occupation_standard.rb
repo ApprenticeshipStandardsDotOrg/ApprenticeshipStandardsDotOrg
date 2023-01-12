371dd13291dd434dbd123844c04c373dea6f6160
Jennifer Lu <nejul33@gmail.com>
2023-01-12 13:53:17 -0500

    Delegate onet and rapids codes and add specs

--- app/models/occupation_standard.rb
+++ app/models/occupation_standard.rb
@@ -1,4 +1,6 @@
 class OccupationStandard < ApplicationRecord
   belongs_to :occupation, optional: true
   belongs_to :registration_agency
+
+  delegate :rapids_code, :onet_code, to: :occupation, allow_nil: true
 end
--- spec/models/occupation_standard_spec.rb
+++ spec/models/occupation_standard_spec.rb
@@ -1,9 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe OccupationStandard, type: :model do
-    it "has a valid factory" do
-      occupation_standard = build(:occupation_standard)
+  it "has a valid factory" do
+    occupation_standard = build(:occupation_standard)
 
-      expect(occupation_standard).to be_valid
+    expect(occupation_standard).to be_valid
+  end
+
+  describe "#rapids_code" do 
+    it "returns occupation rapids_code when occupation exists" do
+      occupation = build_stubbed(:occupation, rapids_code: "abc")
+      occupation_standard = build(:occupation_standard, occupation: occupation)
+
+      expect(occupation_standard.rapids_code).to eq "abc"
     end
+
+    it "returns nil when no occupation" do
+      occupation_standard = build(:occupation_standard, occupation: nil)
+
+      expect(occupation_standard.rapids_code).to be_nil
+    end
+  end
+
+  describe "#onet_code" do 
+    it "returns occupation onet_code when occupation exists" do
+      occupation = build_stubbed(:occupation, onet_code: "abc")
+      occupation_standard = build(:occupation_standard, occupation: occupation)
+
+      expect(occupation_standard.onet_code).to eq "abc"
+    end
+
+    it "returns nil when no occupation" do
+      occupation_standard = build(:occupation_standard, occupation: nil)
+
+      expect(occupation_standard.onet_code).to be_nil
+    end
+  end
 end
