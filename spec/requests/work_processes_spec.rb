df6839e7af5a06c901d812e54f5c5579a31a57ce
Jennifer Lu <nejul33@gmail.com>
2023-01-17 11:28:05 -0500

    WIP work proceess create

--- spec/requests/work_processes_spec.rb
+++ spec/requests/work_processes_spec.rb
@@ -32,4 +32,47 @@ RSpec.describe "WorkProcesses", type: :request do
       end
     end
   end
+
+  describe "POST /create" do
+    context "with valid parameters" do
+      it "creates new work process and redirects to index page" do
+        expect {
+          post work_processes_path, params: {
+            work_process: {
+              title: "Brakes",
+              description: "This is what you should do to count hours against brakes requirements",
+              occupation_standard_id: 1, 
+              default_hours: 100
+            }
+          }
+        }.to change(WorkProcess, :count).by(1)
+
+        wp = WorkProcess.last
+        expect(wp.title).to eq "Brakes"
+        expect(wp.description).to eq "This is what you should do to count hours against brakes requirements"
+        expect(wp.occupation_standard_id).to eq 1
+        expect(wp.default_hours).to eq 100
+
+        expect(response).to redirect_to work_processes_path
+      end
+    end
+
+    # context "with invalid parameters" do
+    #   it "does not create new standards import record and renders new" do
+    #     allow_any_instance_of(StandardsImport).to receive(:save).and_return(false)
+    #     expect {
+    #       post standards_imports_path, params: {
+    #         standards_import: {
+    #           name: "Mickey Mouse",
+    #           email: "mickey@mouse.com",
+    #           organization: "Disney",
+    #           notes: "a" * 500
+    #         }
+    #       }
+    #     }.to_not change(StandardsImport, :count)
+
+    #     expect(response).to have_http_status(:ok)
+    #   end
+    # end
+  end
 end
