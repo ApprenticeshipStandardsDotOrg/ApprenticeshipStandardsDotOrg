f8467fbef365cc88821c7cdce97c3e1698322017
Jennifer Lu <nejul33@gmail.com>
2023-01-18 16:04:58 -0500

    Remove extra migration files

--- db/migrate/20230117192226_add_min_and_max_hours_to_work_process.rb
+++
@@ -1,6 +0,0 @@
-class AddMinAndMaxHoursToWorkProcess < ActiveRecord::Migration[7.0]
-  def change
-    add_column :work_processes, :minimum_hours, "int4range"
-    add_column :work_processes, :maximum_hours, "int4range"
-  end
-end
