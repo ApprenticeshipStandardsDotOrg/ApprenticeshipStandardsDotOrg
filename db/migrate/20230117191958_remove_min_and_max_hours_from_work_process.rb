de34268128787037fe4a2c57fdb68cdba12cf8fd
Jennifer Lu <nejul33@gmail.com>
2023-01-19 10:03:34 -0500

    Make changes to migrations to revert min and max hours to integer

--- db/migrate/20230117191958_remove_min_and_max_hours_from_work_process.rb
+++
@@ -1,6 +0,0 @@
-class RemoveMinAndMaxHoursFromWorkProcess < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :work_processes, :minimum_hours
-    remove_column :work_processes, :maximum_hours
-  end
-end
--- db/migrate/20230118203640_add_hours_to_work_process.rb
+++
@@ -1,5 +0,0 @@
-class AddHoursToWorkProcess < ActiveRecord::Migration[7.0]
-  def change
-    add_column :work_processes, :hours, "int4range"
-  end
-end
