53d8bdc2ecc398f77bc53bd9478d46b9e94f7bb8
Jennifer Lu <nejul33@gmail.com>
2023-01-19 15:53:15 -0500

    Lint

--- spec/models/related_instruction_spec.rb
+++ spec/models/related_instruction_spec.rb
@@ -1,7 +1,7 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe RelatedInstruction, type: :model do
-    it "has a valid factory" do
+  it "has a valid factory" do
     related_instruction = build(:related_instruction)
 
     expect(related_instruction).to be_valid
