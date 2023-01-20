1ef7c3cc3eddf08b003a8a5ac9011d973e2544c3
Jennifer Lu <nejul33@gmail.com>
2023-01-20 14:16:50 -0500

    Lint

--- spec/models/related_instruction_spec.rb
+++ spec/models/related_instruction_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe RelatedInstruction, type: :model do
 
   it "has unique sort_order wrt occupation_standard" do
     related_instruction = create(:related_instruction, sort_order: 1)
-    new_related_instruction = build(:related_instruction, 
+    new_related_instruction = build(:related_instruction,
       occupation_standard: related_instruction.occupation_standard, sort_order: 1)
 
     expect(new_related_instruction).to_not be_valid
