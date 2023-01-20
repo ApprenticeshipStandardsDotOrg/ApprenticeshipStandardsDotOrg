8dcf0bf20a3934d212ba6fe4e40149ef0b50bca9
Jennifer Lu <nejul33@gmail.com>
2023-01-20 14:08:37 -0500

    Add uniqueness validation to sort order

--- app/models/wage_step.rb
+++ app/models/wage_step.rb
@@ -1,3 +1,5 @@
 class WageStep < ApplicationRecord
   belongs_to :occupation_standard
+
+  validates :sort_order, uniqueness: {scope: :occupation_standard}
 end
--- spec/models/wage_step_spec.rb
+++ spec/models/wage_step_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe WageStep, type: :model do
 
     expect(wage_step).to be_valid
   end
+
+  it "has unique sort_order wrt occupation_standard" do
+    wage_step = create(:wage_step, sort_order: 1)
+    new_wage_step = build(:wage_step, occupation_standard: wage_step.occupation_standard, sort_order: 1)
+
+    expect(new_wage_step).to_not be_valid
+    new_wage_step.sort_order = 2
+    expect(new_wage_step).to be_valid
+  end
 end
