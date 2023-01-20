dc17f58c2f630c63b8feade167d8ee9cd5eb2d93
Jennifer Lu <nejul33@gmail.com>
2023-01-20 14:08:54 -0500

    Lint

--- spec/models/wage_step_spec.rb
+++ spec/models/wage_step_spec.rb
@@ -1,7 +1,7 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe WageStep, type: :model do
-    it "has a valid factory" do
+  it "has a valid factory" do
     wage_step = build(:wage_step)
 
     expect(wage_step).to be_valid
