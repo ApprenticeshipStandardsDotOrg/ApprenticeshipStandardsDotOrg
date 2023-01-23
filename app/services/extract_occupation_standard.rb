4f62b2a1c91f310dce03e9814c10a31101d49abf
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-01-23 15:30:02 -0800

    Flesh out creation of occupation standard from data_import file

--- app/services/extract_occupation_standard.rb
+++ app/services/extract_occupation_standard.rb
@@ -1,7 +1,64 @@
 class ExtractOccupationStandard
-  def initialize
+  attr_reader :data_import
+
+  def initialize(data_import)
+    @data_import = data_import
   end
 
   def call
+    data_import.file.open do |file|
+      xlsx = Roo::Spreadsheet.open(file, extension: :xlsx)
+      sheet = xlsx.sheet(0)
+
+      sheet.parse(headers: true).each_with_index do |row, index|
+        next if index.zero?
+
+        state = State.find_by(abbreviation: row["Registration State"])
+        reg_agency = RegistrationAgency.find_by(state: state, agency_type: row["OA or SAA"].downcase.to_sym)
+        organization = if (sponsor_name = row["Sponsor Name"])
+                    Organization.find_or_create_by!(name: sponsor_name)
+                  end
+
+        rapids_code = if (match = row["RAPIDS Code"].match(/(.*)[A-Za-z]{2}\z/))
+                        match.captures.first
+                      end
+
+        occupation_type = case row["Type"]
+                          when /competency/i
+                            :competency
+                          when /time/i
+                            :time
+                          when /hybrid/i
+                            :hybrid
+                          end
+
+        occupation = Occupation.find_by(rapids_code: rapids_code)
+        occupation ||= begin
+                         onet_code = OnetCode.find_by(row["Onet Code"])
+                         if onet_code
+                           Occupation.find_by(onet_code: onet_code)
+                         end
+                       end
+
+        OccupationStandard.create!(
+          data_import: data_import,
+          occupation: occupation,
+          registration_agency: reg_agency,
+          title: row["Occupation Title"],
+          existing_title: row["Existing Title"],
+          term_months: row["Term (in months)"],
+          onet_code: row["Onet Code"],
+          rapids_code: rapids_code,
+          occupation_type: occupation_type,
+          probationary_period_months: row["Probationary Period"],
+          apprenticeship_to_journeyworker_ratio: row["Ratio of Apprentice to Journeyworker"],
+          organization: organization,
+          ojt_hours_min: row["Minimum OJT Hours"],
+          ojt_hours_max: row["Maximum OJT Hours"],
+          rsi_hours_min: row["Minimum RSI Hours"],
+          rsi_hours_max: row["Maximum RSI Hours"],
+        )
+      end
+    end
   end
 end
--- spec/services/extract_occupation_standard_spec.rb
+++ spec/services/extract_occupation_standard_spec.rb
@@ -11,26 +11,28 @@ RSpec.describe ExtractOccupationStandard, type: :service do
       occupation1 = create(:occupation, rapids_code: "1057")
       _occupation2 = create(:occupation, onet_code: onet_code)
 
-      service = described_class.new()
+      data_import = create(:data_import)
+
+      service = described_class.new(data_import)
 
       expect{
         service.call
       }.to change(OccupationStandard, :count).by(1)
-        .and change(Organization, :count).by(1)
+        .and change(Organization, :count).by(0)
 
       os = OccupationStandard.last
+      expect(os.data_import).to eq data_import
       expect(os.occupation).to eq occupation1
       expect(os.registration_agency).to eq ca_oa
       expect(os.title).to eq "HUMAN RESOURCE SPECIALIST"
       expect(os.existing_title).to eq "Career Development Technician"
-      expect(os.term_months).to eq 30
-      expect(os).to be competency_based
+      expect(os.term_months).to eq 12
+      expect(os).to be_competency_based
       expect(os.probationary_period_months).to eq 3
-      expect(os.sponsor_name).to be_nil
       expect(os.onet_code).to eq "13-1071.01"
       expect(os.rapids_code).to eq "1057"
       expect(os.apprenticeship_to_journeyworker_ratio).to eq "5:1"
-      expect(os.organization).to eq Organization.last
+      expect(os.organization).to be_nil
       expect(os.ojt_hours_min).to be_nil
       expect(os.ojt_hours_max).to be_nil
       expect(os.rsi_hours_min).to be_nil
