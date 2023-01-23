3e813d394d5296899b7b907f595e8a0b02f75f13
Fernando Perales <fer@thoughtbot.com>
2023-01-23 12:24:52 -0600

    Group lines per function

--- app/models/occupation.rb
+++ app/models/occupation.rb
@@ -1,7 +1,6 @@
 class Occupation < ApplicationRecord
   validates :name, presence: true
+  has_many :competency_options, as: :resource
 
   belongs_to :onet_code, optional: true
-
-  has_many :competency_options, as: :resource
 end
