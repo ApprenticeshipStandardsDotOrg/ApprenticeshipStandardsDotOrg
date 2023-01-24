99debd5465fa0d64a2736d99a8eb441efd778acd
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-01-24 13:19:08 -0800

    Flesh out background job spec

--- spec/jobs/process_data_import_job_spec.rb
+++ spec/jobs/process_data_import_job_spec.rb
@@ -13,29 +13,71 @@ RSpec.describe ProcessDataImportJob, type: :job do
     end
 
     it "creates an occupation standard when new record" do
-      data_import = build_stubbed(:data_import)
-      occupation_standard = build(:occupation_standard)
-      service = instance_double("ExtractOccupationStandard")
+      ca = create(:state, abbreviation: "CA")
+      ca_oa = create(:registration_agency, state: ca, agency_type: :oa)
+      _ca_saa = create(:registration_agency, state: ca, agency_type: :saa)
 
-      allow(ExtractOccupationStandard).to receive(:new).with(data_import).and_return(service)
-      allow(service).to receive(:call).and_return(occupation_standard)
+      onet_code = create(:onet_code, code: "13-1071.01")
+      occupation1 = create(:occupation, rapids_code: "1057")
+      _occupation2 = create(:occupation, onet_code: onet_code)
+
+      data_import = create(:data_import)
 
       expect {
         described_class.new.perform(data_import)
       }.to change(OccupationStandard, :count).by(1)
+        .and change(Organization, :count).by(0)
+
+      occupation_standard = OccupationStandard.last
+      expect(occupation_standard.data_import).to eq data_import
+      expect(occupation_standard.occupation).to eq occupation1
+      expect(occupation_standard.registration_agency).to eq ca_oa
+      expect(occupation_standard.title).to eq "HUMAN RESOURCE SPECIALIST"
+      expect(occupation_standard.existing_title).to eq "Career Development Technician"
+      expect(occupation_standard.term_months).to eq 12
+      expect(occupation_standard).to be_competency_based
+      expect(occupation_standard.probationary_period_months).to eq 3
+      expect(occupation_standard.onet_code).to eq "13-1071.01"
+      expect(occupation_standard.rapids_code).to eq "1057"
+      expect(occupation_standard.apprenticeship_to_journeyworker_ratio).to eq "5:1"
+      expect(occupation_standard.organization).to be_nil
+      expect(occupation_standard.ojt_hours_min).to be_nil
+      expect(occupation_standard.ojt_hours_max).to be_nil
+      expect(occupation_standard.rsi_hours_min).to be_nil
+      expect(occupation_standard.rsi_hours_max).to be_nil
     end
 
     it "updates an occupation standard when it exists" do
+      ca = create(:state, abbreviation: "CA")
+      ca_oa = create(:registration_agency, state: ca, agency_type: :oa)
+
+      onet_code = create(:onet_code, code: "13-1071.01")
+      occupation = create(:occupation, rapids_code: "1057")
+
       data_import = create(:data_import)
       occupation_standard = create(:occupation_standard, data_import: data_import)
-      service = instance_double("ExtractOccupationStandard")
-
-      allow(ExtractOccupationStandard).to receive(:new).with(data_import).and_return(service)
-      allow(service).to receive(:call).and_return(occupation_standard)
 
       expect {
         described_class.new.perform(data_import)
       }.to_not change(OccupationStandard, :count)
+
+      occupation_standard.reload
+      expect(occupation_standard.data_import).to eq data_import
+      expect(occupation_standard.occupation).to eq occupation
+      expect(occupation_standard.registration_agency).to eq ca_oa
+      expect(occupation_standard.title).to eq "HUMAN RESOURCE SPECIALIST"
+      expect(occupation_standard.existing_title).to eq "Career Development Technician"
+      expect(occupation_standard.term_months).to eq 12
+      expect(occupation_standard).to be_competency_based
+      expect(occupation_standard.probationary_period_months).to eq 3
+      expect(occupation_standard.onet_code).to eq "13-1071.01"
+      expect(occupation_standard.rapids_code).to eq "1057"
+      expect(occupation_standard.apprenticeship_to_journeyworker_ratio).to eq "5:1"
+      expect(occupation_standard.organization).to be_nil
+      expect(occupation_standard.ojt_hours_min).to be_nil
+      expect(occupation_standard.ojt_hours_max).to be_nil
+      expect(occupation_standard.rsi_hours_min).to be_nil
+      expect(occupation_standard.rsi_hours_max).to be_nil
     end
   end
 end
