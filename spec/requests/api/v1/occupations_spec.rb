655d8bcdb5d0d4cf63c851653444dea1de6e10fe
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-01-31 10:55:50 -0800

    Add spec for retrieving a single occupation

--- spec/requests/api/v1/occupations_spec.rb
+++ spec/requests/api/v1/occupations_spec.rb
@@ -3,9 +3,9 @@ require "swagger_helper"
 RSpec.describe "api/v1/occupations", type: :request do
   path "/api/v1/occupations" do
     get "List occupations" do
-      response(200, "successful") do
-        produces "application/vnd.api+json"
+      produces "application/vnd.api+json"
 
+      response(200, "successful") do
         let(:onet_code1) { create(:onet_code, code: "51-7011.00") }
         let!(:occ1) { create(:occupation, name: "Information Technology Specialist", onet_code: onet_code1, rapids_code: "1132", time_based_hours: 2782, competency_based_hours: 2000) }
         let!(:occ2) { create(:occupation, name: "Accordion Maker", rapids_code: "0860", time_based_hours: 8000, competency_based_hours: 8500) }
@@ -25,6 +25,13 @@ RSpec.describe "api/v1/occupations", type: :request do
                 properties: {
                   id: {type: :string},
                   type: {type: :string},
+                  links: {
+                    type: :object,
+                    properties: {
+                      self: {type: :string}
+                    },
+                    required: %w[self]
+                  },
                   attributes: {
                     type: :object,
                     properties: {
@@ -37,7 +44,7 @@ RSpec.describe "api/v1/occupations", type: :request do
                     required: %w[name]
                   }
                 },
-                required: %w[id type attributes]
+                required: %w[id type attributes links]
               }
             }
           },
@@ -75,6 +82,77 @@ RSpec.describe "api/v1/occupations", type: :request do
                 }
               }
             ]
+          }
+
+          expect(json_response).to eq expected_resp
+        end
+      end
+    end
+  end
+
+  path "/api/v1/occupations/{id}" do
+    get "Retrieve occupation" do
+      parameter name: :id, in: :path, type: :string
+      produces "application/vnd.api+json"
+
+      response(200, "successful") do
+        let(:onet_code1) { create(:onet_code, code: "51-7011.00") }
+        let!(:occ) { create(:occupation, name: "Information Technology Specialist", onet_code: onet_code1, rapids_code: "1132", time_based_hours: 2782, competency_based_hours: 2000) }
+        let(:id) { occ.id }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            "application/vnd.api+json" => {example: response_json}
+          }
+        end
+
+        schema type: :object,
+          properties: {
+            data: {
+              type: :object,
+              properties: {
+                id: {type: :string},
+                type: {type: :string},
+                links: {
+                  type: :object,
+                  properties: {
+                    self: {type: :string}
+                  },
+                  required: %w[self]
+                },
+                attributes: {
+                  type: :object,
+                  properties: {
+                    name: {type: :string},
+                    onet_code: {type: :string, nullable: true},
+                    rapids_code: {type: :string, nullable: true},
+                    time_based_hours: {type: :integer, nullable: true},
+                    competency_based_hours: {type: :integer, nullable: true}
+                  },
+                  required: %w[name]
+                },
+                required: %w[id type attributes links]
+              }
+            }
+          },
+          required: %w[data]
+
+        run_test! do |response|
+          expected_resp = {
+            data: {
+              id: occ.id.to_s,
+              type: "occupations",
+              links: {
+                self: api_v1_occupation_url(occ)
+              },
+              attributes: {
+                name: occ.name,
+                onet_code: "51-7011.00",
+                rapids_code: "1132",
+                time_based_hours: 2782,
+                competency_based_hours: 2000
+              }
+            }
           }
 
           expect(json_response).to eq expected_resp
