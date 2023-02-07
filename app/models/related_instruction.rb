dd9acf97ddd51e70efbc4c98ec759f2189f64c98
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-02-07 06:57:35 -0800

    Add presence validations for some fields on RelatedInstruction model (#54)

--- app/models/related_instruction.rb
+++ app/models/related_instruction.rb
@@ -3,7 +3,8 @@ class RelatedInstruction < ApplicationRecord
   belongs_to :organization, optional: true
   belongs_to :default_course, class_name: "Course", optional: true
 
-  validates :sort_order, uniqueness: {scope: [:occupation_standard, :title, :code]}
+  validates :sort_order, presence: true, uniqueness: {scope: [:occupation_standard, :title, :code]}
+  validates :title, presence: true
 
   delegate :title, :description, :code, :hours, to: :default_course, prefix: true
   delegate :title, to: :organization, prefix: true
