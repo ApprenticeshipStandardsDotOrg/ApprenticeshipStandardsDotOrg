cb12bedbc6499334a8f295ee90c9ea78246557b8
Jennifer Lu <58493319+jenlu33@users.noreply.github.com>
2023-02-07 12:08:52 -0800

    Add view spec for file_imports#edit (#15)
    Co-authored-by: Jeanine Soterwood <jeanine@thoughtbot.com>

---
+++ spec/views/file_imports/edit_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe "file_imports/edit.html.erb", type: :view do
+  it "has heading text and form" do
+    file_import = create(:file_import)
+    assign(:file_import, file_import)
+
+    render
+
+    expect(rendered).to have_selector("h1", text: "Edit #{file_import.filename}")
+    expect(rendered).to have_field("Status")
+    expect(rendered).to have_button("Update")
+  end
+end
