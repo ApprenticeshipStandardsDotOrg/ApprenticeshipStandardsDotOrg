cf9e359960958f3e850344b46a91f0eac4da4faa
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-02-08 06:12:05 -0800

    Process data_import file after upload

--- app/models/data_import.rb
+++ app/models/data_import.rb
@@ -5,11 +5,17 @@ class DataImport < ApplicationRecord
 
   validate :file_presence
 
+  after_commit :process_data_import, on: :create
+
   private
 
   def file_presence
     unless file.attached?
       errors.add(:file, "must be attached")
     end
+  end
+
+  def process_data_import
+    ProcessDataImportJob.perform_later(self)
   end
 end
