5ea34d755e7d06cdc65138b4609dcb3e22cc5b16
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-02-08 10:12:58 -0800

    Hardcode protocol in URI.open to avoid:
    'Security/Open: The use of  is a serious security risk' error

--- app/jobs/scraper/california_job.rb
+++ app/jobs/scraper/california_job.rb
@@ -2,28 +2,33 @@ class Scraper::CaliforniaJob < ApplicationJob
   queue_as :default
 
   def perform
-    url_base = "https://www.dir.ca.gov/das/"
-    fetch_url = url_base + "ProgramStandards.htm"
+    protocol = "https://"
+    url_base = "www.dir.ca.gov/das/"
+    fetch_url = protocol + url_base + "ProgramStandards.htm"
+
     uri = URI.parse(fetch_url)
     response = Net::HTTP.get_response(uri)
     html = response.body
-    doc = Nokogiri::HTML(html)
 
+    doc = Nokogiri::HTML(html)
     nodeset = doc.css(".main-primary a[href]")
     file_paths = nodeset.map { |element| element["href"] }.select { |href| href.ends_with?(".pdf") }
 
     file_paths.each do |path|
-      full_path = url_base + path
-      full_path.gsub!(/\s/, "%20")
+      query = url_base + path
+      query.gsub!(/\s/, "%20")
 
       standards_import = StandardsImport.where(
-        name: full_path,
+        name: protocol + query,
         organization: fetch_url
       ).first_or_create!(
         notes: "From Scraper::CaliforniaJob"
       )
 
-      standards_import.files.attach(io: URI.open(full_path), filename: File.basename(path))
+      # protocol needs to be hardcoded to avoid Rubocop error:
+      # Security/Open: The use of `URI.open` is a serious security risk
+      # https://github.com/rubocop/rubocop/issues/6216#issuecomment-1252449855
+      standards_import.files.attach(io: URI.open("https://#{query}"), filename: File.basename(path))
     end
   end
 end
