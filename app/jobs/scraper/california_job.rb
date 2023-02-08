213c89a71b506468181ec4bf731d8c6770b44436
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-02-08 10:06:54 -0800

    lint

--- app/jobs/scraper/california_job.rb
+++ app/jobs/scraper/california_job.rb
@@ -3,14 +3,14 @@ class Scraper::CaliforniaJob < ApplicationJob
 
   def perform
     url_base = "https://www.dir.ca.gov/das/"
-    fetch_url =  url_base + "ProgramStandards.htm"
+    fetch_url = url_base + "ProgramStandards.htm"
     uri = URI.parse(fetch_url)
     response = Net::HTTP.get_response(uri)
     html = response.body
     doc = Nokogiri::HTML(html)
 
     nodeset = doc.css(".main-primary a[href]")
-    file_paths = nodeset.map{|element| element["href"]}.select{|href| href.ends_with?(".pdf")}
+    file_paths = nodeset.map { |element| element["href"] }.select { |href| href.ends_with?(".pdf") }
 
     file_paths.each do |path|
       full_path = url_base + path
@@ -18,7 +18,7 @@ class Scraper::CaliforniaJob < ApplicationJob
 
       standards_import = StandardsImport.where(
         name: full_path,
-        organization: fetch_url,
+        organization: fetch_url
       ).first_or_create!(
         notes: "From Scraper::CaliforniaJob"
       )
--- spec/jobs/scraper/california_job_spec.rb
+++ spec/jobs/scraper/california_job_spec.rb
@@ -30,11 +30,11 @@ RSpec.describe Scraper::CaliforniaJob, type: :job do
 
   def stub_responses
     html_file = file_fixture("scraper/california.htm")
-    stub_request(:get, /dir.ca.gov.*\.htm/).
-      to_return(status: 200, body: html_file.read, headers: {})
+    stub_request(:get, /dir.ca.gov.*\.htm/)
+      .to_return(status: 200, body: html_file.read, headers: {})
 
     pdf_file = file_fixture("pixel1x1.pdf")
-    stub_request(:get, /dir.ca.gov.*\.pdf/).
-      to_return(status: 200, body: pdf_file.read, headers: {})
+    stub_request(:get, /dir.ca.gov.*\.pdf/)
+      .to_return(status: 200, body: pdf_file.read, headers: {})
   end
 end
