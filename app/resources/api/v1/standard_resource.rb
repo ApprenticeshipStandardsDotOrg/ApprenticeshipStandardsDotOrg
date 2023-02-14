6b5fe7573c9a4ec9b7b2282727e33d65e035a19c
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-02-14 11:10:18 -0800

    Add ability to filter standards by title

--- app/resources/api/v1/standard_resource.rb
+++ app/resources/api/v1/standard_resource.rb
@@ -4,6 +4,8 @@ class API::V1::StandardResource < JSONAPI::Resource
 
   attributes :title, :existing_title, :sponsor_name, :registration_agency, :onet_code, :rapids_code, :ojt_type, :term_months, :probationary_period_months, :apprenticeship_to_journeyworker_ratio, :ojt_hours_min, :ojt_hours_max, :rsi_hours_min, :rsi_hours_max
 
+  filter :title
+
   def ojt_type
     @model.ojt_type + "_based"
   end
--- spec/requests/api/v1/standards_spec.rb
+++ spec/requests/api/v1/standards_spec.rb
@@ -3,49 +3,50 @@ require "swagger_helper"
 RSpec.describe "api/v1/standards", type: :request do
   path "/api/v1/standards" do
     get "List standards" do
+      parameter name: :"filter[title]", in: :query, type: :string, required: false, description: "Filter by title"
       produces "application/vnd.api+json"
 
-      response(200, "successful") do
-        let(:ca_state) { create(:state, name: "California") }
-        let(:registration_agency) { create(:registration_agency, agency_type: :saa, state: ca_state) }
-        let!(:standard1) {
-          create(
-            :occupation_standard,
-            registration_agency: registration_agency,
-            organization: build(:organization, title: "HR Industries, Inc"),
-            title: "Human Resource Specialist",
-            existing_title: "Career Development Technician",
-            term_months: 12,
-            ojt_type: :time,
-            probationary_period_months: 6,
-            onet_code: "51-7011.00",
-            rapids_code: "0857",
-            apprenticeship_to_journeyworker_ratio: "5:1",
-            ojt_hours_min: 100,
-            ojt_hours_max: 150,
-            rsi_hours_min: 300,
-            rsi_hours_max: 350
-          )
-        }
-        let!(:standard2) {
-          create(
-            :occupation_standard,
-            registration_agency: registration_agency,
-            title: "Automotive Technician Specialist",
-            existing_title: nil,
-            term_months: 24,
-            ojt_type: :competency,
-            probationary_period_months: 12,
-            onet_code: "49-3023.02",
-            rapids_code: "1034",
-            apprenticeship_to_journeyworker_ratio: "1:1",
-            ojt_hours_min: 1000,
-            ojt_hours_max: 1500,
-            rsi_hours_min: 200,
-            rsi_hours_max: 250
-          )
-        }
+      let(:ca_state) { create(:state, name: "California") }
+      let(:registration_agency) { create(:registration_agency, agency_type: :saa, state: ca_state) }
+      let!(:standard1) {
+        create(
+          :occupation_standard,
+          registration_agency: registration_agency,
+          organization: build(:organization, title: "HR Industries, Inc"),
+          title: "Human Resource Specialist",
+          existing_title: "Career Development Technician",
+          term_months: 12,
+          occupation_type: :time,
+          probationary_period_months: 6,
+          onet_code: "51-7011.00",
+          rapids_code: "0857",
+          apprenticeship_to_journeyworker_ratio: "5:1",
+          ojt_hours_min: 100,
+          ojt_hours_max: 150,
+          rsi_hours_min: 300,
+          rsi_hours_max: 350
+        )
+      }
+      let!(:standard2) {
+        create(
+          :occupation_standard,
+          registration_agency: registration_agency,
+          title: "Automotive Technician Specialist",
+          existing_title: nil,
+          term_months: 24,
+          occupation_type: :competency,
+          probationary_period_months: 12,
+          onet_code: "49-3023.02",
+          rapids_code: "1034",
+          apprenticeship_to_journeyworker_ratio: "1:1",
+          ojt_hours_min: 1000,
+          ojt_hours_max: 1500,
+          rsi_hours_min: 200,
+          rsi_hours_max: 250
+        )
+      }
 
+      response(200, "successful") do
         after do |example|
           example.metadata[:response][:content] = {
             "application/vnd.api+json" => {example: response_json}
@@ -77,7 +78,7 @@ RSpec.describe "api/v1/standards", type: :request do
                       registration_agency: {type: :string},
                       onet_code: {type: :string, nullable: true},
                       rapids_code: {type: :string, nullable: true},
-                      ojt_type: {type: :string},
+                      occupation_type: {type: :string},
                       term_months: {type: :integer, nullable: true},
                       probationary_period_months: {type: :integer, nullable: true},
                       apprenticeship_to_journeyworker_ratio: {type: :string, nullable: true},
@@ -127,7 +128,7 @@ RSpec.describe "api/v1/standards", type: :request do
                   registration_agency: "California (SAA)",
                   onet_code: "49-3023.02",
                   rapids_code: "1034",
-                  ojt_type: "competency_based",
+                  occupation_type: "competency_based",
                   term_months: 24,
                   probationary_period_months: 12,
                   apprenticeship_to_journeyworker_ratio: "1:1",
@@ -150,7 +151,7 @@ RSpec.describe "api/v1/standards", type: :request do
                   registration_agency: "California (SAA)",
                   onet_code: "51-7011.00",
                   rapids_code: "0857",
-                  ojt_type: "time_based",
+                  occupation_type: "time_based",
                   term_months: 12,
                   probationary_period_months: 6,
                   apprenticeship_to_journeyworker_ratio: "5:1",
@@ -164,6 +165,44 @@ RSpec.describe "api/v1/standards", type: :request do
           }
 
           expect(response_json).to eq expected_resp
+        end
+      end
+
+      context "filter by title" do
+        response(200, "success", document: false) do
+          let("filter[title]") { "Human Resource Specialist" }
+
+          run_test! do |response|
+            expected_resp = {
+              data: [
+                {
+                  id: standard1.id.to_s,
+                  type: "standards",
+                  links: {
+                    self: api_v1_standard_url(standard1)
+                  },
+                  attributes: {
+                    title: "Human Resource Specialist",
+                    existing_title: "Career Development Technician",
+                    sponsor_name: "HR Industries, Inc",
+                    registration_agency: "California (SAA)",
+                    onet_code: "51-7011.00",
+                    rapids_code: "0857",
+                    occupation_type: "time_based",
+                    term_months: 12,
+                    probationary_period_months: 6,
+                    apprenticeship_to_journeyworker_ratio: "5:1",
+                    ojt_hours_min: 100,
+                    ojt_hours_max: 150,
+                    rsi_hours_min: 300,
+                    rsi_hours_max: 350
+                  }
+                }
+              ]
+            }
+
+            expect(response_json).to eq expected_resp
+          end
         end
       end
     end
@@ -185,7 +224,7 @@ RSpec.describe "api/v1/standards", type: :request do
             title: "Human Resource Specialist",
             existing_title: "Career Development Technician",
             term_months: 12,
-            ojt_type: :time,
+            occupation_type: :time,
             probationary_period_months: 6,
             onet_code: "51-7011.00",
             rapids_code: "0857",
@@ -227,7 +266,7 @@ RSpec.describe "api/v1/standards", type: :request do
                     registration_agency: {type: :string},
                     onet_code: {type: :string, nullable: true},
                     rapids_code: {type: :string, nullable: true},
-                    ojt_type: {type: :string},
+                    occupation_type: {type: :string},
                     term_months: {type: :integer, nullable: true},
                     probationary_period_months: {type: :integer, nullable: true},
                     apprenticeship_to_journeyworker_ratio: {type: :string, nullable: true},
@@ -275,7 +314,7 @@ RSpec.describe "api/v1/standards", type: :request do
                 registration_agency: "California (SAA)",
                 onet_code: "51-7011.00",
                 rapids_code: "0857",
-                ojt_type: "time_based",
+                occupation_type: "time_based",
                 term_months: 12,
                 probationary_period_months: 6,
                 apprenticeship_to_journeyworker_ratio: "5:1",
