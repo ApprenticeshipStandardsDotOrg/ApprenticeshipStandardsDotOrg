edad66e0f68dbab1ed7871f3ef1c491186dbf05a
Fernando Perales <fer@thoughtbot.com>
2023-02-21 10:40:36 -0600

    Add spec to test file without attachment is ignored

--- spec/jobs/scraper/apprenticeship_bulletins_job_spec.rb
+++ spec/jobs/scraper/apprenticeship_bulletins_job_spec.rb
@@ -3,17 +3,30 @@ require "rails_helper"
 RSpec.describe Scraper::AppreticeshipBulletinsJob, type: :job do
   describe "#perform" do
     context "when files have not been downloaded previously" do
-      it "downloads docx files to a standards import record" do
-        stub_responses
-        expect {
-          described_class.new.perform
-        }.to change(StandardsImport, :count).by(3)
+      context "when file has attachments" do
+        it "downloads docx files to a standards import record" do
+          stub_responses
+          expect {
+            described_class.new.perform
+          }.to change(StandardsImport, :count).by(3)
 
-        standard_import = StandardsImport.last
-        expect(standard_import.files.count).to eq 1
-        expect(standard_import.name).to eq "https://www.apprenticeship.gov/sites/default/files/bulletins/Bulletin%202023-50%20New%20NOF%20Appren%20Occ%20Jr%20Cloud%20Engineer.docx"
-        expect(standard_import.organization).to eq "2023-50"
-        expect(standard_import.notes).to eq "From Scraper::AppreticeshipBulletinsJob #{Scraper::AppreticeshipBulletinsJob::BULLETIN_LIST_URL}"
+          standard_import = StandardsImport.last
+          expect(standard_import.files.count).to eq 1
+          expect(standard_import.name).to eq "https://www.apprenticeship.gov/sites/default/files/bulletins/Bulletin%202023-50%20New%20NOF%20Appren%20Occ%20Jr%20Cloud%20Engineer.docx"
+          expect(standard_import.organization).to eq "2023-50"
+          expect(standard_import.notes).to eq "From Scraper::AppreticeshipBulletinsJob #{Scraper::AppreticeshipBulletinsJob::BULLETIN_LIST_URL}"
+        end
+      end
+
+      context "when file does not have attachments" do
+        it "ignores the file" do
+          stub_responses_for_file_without_attachments
+          expect {
+            described_class.new.perform
+          }.to_not change(StandardsImport, :count)
+
+          expect(StandardsImport.last).to be_nil
+        end
       end
     end
 
@@ -39,7 +52,7 @@ RSpec.describe Scraper::AppreticeshipBulletinsJob, type: :job do
       .to_return(status: 200, body: bulletin_document.read, headers: {})
 
     first_bulletin = file_fixture("scraper/bulletins/Bulletin 2023-52 New NGS AFSA.docx")
-    stub_request(:get, /Bulletin 2023-52 New NGS AFSA.docx/)
+    stub_request(:get, /Bulletin%202023-52%20New%20NGS%20AFSA.docx/)
       .to_return(status: 200, body: first_bulletin, headers: {"Content-Type": Scraper::AppreticeshipBulletinsJob::DOCX_CONTENT_TYPE})
 
     second_bulletin = file_fixture("scraper/bulletins/Bulletin 2023-51 New Appren Occ Appraisal and Valuation of Real Property.docx")
@@ -49,5 +62,15 @@ RSpec.describe Scraper::AppreticeshipBulletinsJob, type: :job do
     third_bulletin = file_fixture("scraper/bulletins/Bulletin 2023-50 New NOF Appren Occ Jr Cloud Engineer.docx")
     stub_request(:get, /Bulletin%202023-50%20New%20NOF%20Appren%20Occ%20Jr%20Cloud%20Engineer.docx/)
       .to_return(status: 200, body: third_bulletin, headers: {"Content-Type": Scraper::AppreticeshipBulletinsJob::DOCX_CONTENT_TYPE})
+  end
+
+  def stub_responses_for_file_without_attachments
+    bulletin_document = file_fixture("scraper/bulletins-results-without-attachments.csv")
+    stub_request(:get, Scraper::AppreticeshipBulletinsJob::BULLETIN_LIST_URL)
+      .to_return(status: 200, body: bulletin_document.read, headers: {})
+
+    first_bulletin = file_fixture("scraper/bulletins/Bulletin 2023-46 ManpowerGroup Rev Appendix A.docx")
+    stub_request(:get, /Bulletin%202023-46%20ManpowerGroup%20Rev%20Appendix%20A.docx/)
+      .to_return(status: 200, body: first_bulletin, headers: {"Content-Type": Scraper::AppreticeshipBulletinsJob::DOCX_CONTENT_TYPE})
   end
 end
