cc56702bea7b2926d4e06760c813603fee754b8c
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-03-07 09:46:56 -0800

    Add system specs for data_imports new, edit pages

---
+++ spec/system/data_imports/edit_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe "data_imports/edit" do
+  it "allows admin user to edit data import", :admin do
+    data_import = create(:data_import)
+    file_import = data_import.file_import
+    admin = create :admin
+
+    login_as admin
+    visit edit_file_import_data_import_path(file_import, data_import)
+
+    expect(page).to have_content("Edit #{data_import.file.filename}")
+    fill_in "Description", with: "New desc"
+
+    click_on "Update"
+
+    within("h1") do
+      expect(page).to have_content("Data Import")
+    end
+    expect(page).to have_content("Description")
+    expect(page).to have_content("New desc")
+    expect(page).to have_content("File")
+    expect(page).to have_content(data_import.file.filename)
+    expect(page).to have_content("Occupation standard")
+    expect(page).to have_content(data_import.occupation_standard.title)
+  end
+end
