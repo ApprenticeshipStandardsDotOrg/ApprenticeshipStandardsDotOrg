72bd80eb4d01d58d22b1c7574469d2ef032bb8b7
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-03-09 11:30:46 -0800

    lint

--- spec/system/source_files/show_spec.rb
+++ spec/system/source_files/show_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "source_files/show" do
     login_as admin
     visit source_file_path(source_file)
 
-    expect(page).to have_content "Source file: #{source_file.filename}" 
+    expect(page).to have_content "Source file: #{source_file.filename}"
 
     expect(page).to have_link "Edit", href: edit_source_file_data_import_path(source_file, data_import1)
     expect(page).to have_link "occupation-standards-template.xlsx", href: source_file_data_import_path(source_file, data_import1)
