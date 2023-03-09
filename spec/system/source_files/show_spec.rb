9f8d9aebfb42a544f1b32c83d27d84ca95ef73b1
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-03-09 11:44:13 -0800

    DataImport may not have an occupation standard

--- spec/system/source_files/show_spec.rb
+++ spec/system/source_files/show_spec.rb
@@ -8,8 +8,7 @@ RSpec.describe "source_files/show" do
     occupation_standard1 = create(:occupation_standard, title: "Mechanic")
     data_import1 = create(:data_import, source_file: source_file, occupation_standard: occupation_standard1)
 
-    occupation_standard2 = create(:occupation_standard, title: "Pipe Fitter")
-    data_import2 = create(:data_import, :hybrid, source_file: source_file, occupation_standard: occupation_standard2)
+    data_import2 = create(:data_import, :hybrid, source_file: source_file, occupation_standard: nil)
 
     login_as admin
     visit source_file_path(source_file)
@@ -21,8 +20,6 @@ RSpec.describe "source_files/show" do
     expect(page).to have_link "Mechanic", href: occupation_standard_path(occupation_standard1)
 
     expect(page).to have_link "Edit", href: edit_source_file_data_import_path(source_file, data_import2)
-    expect(page).to have_link "comp-occupation-standards-template.xlsx", href: source_file_data_import_path(source_file, data_import2)
-    expect(page).to have_link "Pipe Fitter", href: occupation_standard_path(occupation_standard2)
 
     expect(page).to have_link "New data import", href: new_source_file_data_import_path(source_file)
   end
