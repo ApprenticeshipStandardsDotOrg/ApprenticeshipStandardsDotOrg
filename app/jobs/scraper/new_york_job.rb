eddcf8e45f7db0dbba41ffd5f51b6991c8dd70d2
Jennifer Lu <nejul33@gmail.com>
2023-03-09 14:56:27 -0500

    Adjust for comments

--- app/jobs/scraper/new_york_job.rb
+++ app/jobs/scraper/new_york_job.rb
@@ -3,23 +3,23 @@ class Scraper::NewYorkJob < ApplicationJob
 
   def perform
     url = "https://dol.ny.gov/apprenticeship/apprenticeship-trades"
+    download_url = "https://dol.ny.gov/system/files/documents/2022/06"
     uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
     html = response.body
     doc = Nokogiri::HTML(html)
 
-    table_and_nav = doc.css("div.wysiwyg--field-webny-wysiwyg-body")
-    table = table_and_nav.css("tbody")[0]
+    table = doc.css("tbody")[0]
     table.css("tr").each do |row|
       next unless row.css("a").present?
       file_name = row.css("a").first["href"]
       file_path = ""
       if /https/.match?(file_name)
-        file_path = file_name.gsub("https://dol.ny.gov/system/files/documents/2022/06", "").gsub(".pdf", "")
+        file_path = file_name.gsub(/#{download_url}/, "").gsub(".pdf", "")
       else
-        file_name
+        file_path = file_name
       end
-
+      
       standards_import = StandardsImport.where(
         name: file_name
       ).first_or_initialize(
@@ -29,11 +29,11 @@ class Scraper::NewYorkJob < ApplicationJob
       if standards_import.new_record?
         begin
           standards_import.files.attach(
-            io: URI.open("https://www.dol.ny.gov/system/files/documents/2022/06#{file_path}.pdf"),
+            io: URI.open("#{download_url}#{file_path}.pdf"),
             filename: File.basename(file_name)
           )
           standards_import.save!
-        rescue
+        rescue OpenURI::HTTPError
           next
         end
       end
