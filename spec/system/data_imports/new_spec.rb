d60150b6af16d04f3e623c247d6ba04bd6600fce
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-03-09 10:18:14 -0800

    Add instruction block on data_import new page

--- spec/system/data_imports/new_spec.rb
+++ spec/system/data_imports/new_spec.rb
@@ -2,15 +2,19 @@ require "rails_helper"
 
 RSpec.describe "data_imports/new" do
   it "allows admin user to create data import", :admin do
-    source_file = create(:source_file)
+    create(:standards_import, :with_files)
+    source_file = SourceFile.first
     admin = create :admin
 
     login_as admin
     visit new_source_file_data_import_path(source_file)
 
     within("h1") do
-      expect(page).to have_content("Upload Occupation Standards")
+      expect(page).to have_content("Import data for source file: pixel1x1.pdf")
     end
+    expect(page).to have_link("import template", href: "#")
+    expect(page).to have_link("ApprenticeshipStandards Notion", href: "https://www.notion.so/Instructions-060de1705e7d471fa8bee7a7c535a2d6")
+
     fill_in "Description", with: "Some desc"
     attach_file "File", "spec/fixtures/files/pixel1x1.pdf"
 
