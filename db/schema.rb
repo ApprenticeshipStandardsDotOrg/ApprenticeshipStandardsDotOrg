a9ea61a4ee2cabcb04bb61a7a4d407fdc96e68a3
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-03-16 14:04:30 -0700

    Add status field to data_imports table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_16_204056) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_16_210337) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -82,6 +82,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_204056) do
     t.datetime "updated_at", null: false
     t.uuid "occupation_standard_id"
     t.uuid "source_file_id", null: false
+    t.integer "status", default: 0, null: false
     t.index ["occupation_standard_id"], name: "index_data_imports_on_occupation_standard_id"
     t.index ["source_file_id"], name: "index_data_imports_on_source_file_id"
     t.index ["user_id"], name: "index_data_imports_on_user_id"
