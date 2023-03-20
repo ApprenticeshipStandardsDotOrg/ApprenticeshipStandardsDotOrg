23e51ae2d16840e47fa31aec7b56d19022cd3f8e
Fatima Burke <fatimaburke@fatimaswordmbp.mynetworksettings.com>
2023-03-20 16:13:21 -0400

    home page general structure

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,6 +1,8 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  darkMode: 'class',
+  
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
@@ -9,8 +11,11 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a"}
+      },
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
       },
     },
   },
