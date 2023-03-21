96581c7e8f147e3f5ea2cdd975494f2d9bbf3b4f
Jennifer Lu <nejul33@gmail.com>
2023-03-21 14:03:57 -0400

    Create washington scraper

--- app/jobs/scraper/washington_job.rb
+++ app/jobs/scraper/washington_job.rb
@@ -3,33 +3,53 @@ class Scraper::WashingtonJob < ApplicationJob
 
   def perform
     url_base = "https://secure.lni.wa.gov/arts-public/"
-    browser = Watir::Browser.new
+    browser = Watir::Browser.new :chrome,
+      options: {args: %w[--headless --no-sandbox --disable-dev-shm-usage --disable-gpu]}
     browser.goto(url_base + "#/program-search")
     js_doc = browser.element(css: "div.lni-u-flex.lni-u-flex-wrap.lni-u-items-end").wait_until(&:present?)
     js_doc.button(text: "Search").click
-    puts "clicked"
 
-    find_table = browser.element(css: "tbody").wait_until(&:present?)
-    table = Nokogiri::HTML(find_table.inner_html)
-    table.css("tr").each do |row|
-      organization = row.css("td.lni-u-text--left > a").first.content
-      puts organization
-      program_path = row.css("td.lni-u-text--center > a").first["href"]
-      puts "*"*10
+    next_button = browser.a(aria_label: "Next").wait_until(&:present?)
+    program_ids = []
 
-      # browser.goto(url_base + program_path)
-      # find_file = browser.element(css: "div.lni-u-mv2 > ol > li")
-      # file = Nokogiri::HTML(find_file.inner_html)
-      # file_link = file.css("a").first["href"]
+    loop do
+      find_table = browser.element(css: "tbody").wait_until(&:present?)
 
-      # standards_import = StandardsImport.where(
-      #   name: url_base + program_path,
-      #   organization: organization
-      # )
+      begin
+        table = Nokogiri::HTML(find_table.inner_html)
+      rescue Watir::Wait::TimeoutError
+        sleep 60
+        retry
+      end
+
+      table.css("tr").each do |row|
+        program_id = row.css("td.lni-u-text--center > a").first.content.strip
+        program_ids << program_id
+      end
+
+      next_button.click!
+      break if browser.a(aria_label: "Next", disabled: true).present?
     end
 
-    # Need to select rows per page and click 50
-    # Click next button and iterate through all rows each time
-    # Next page until button is disabled?
+    program_ids.uniq.each do |id|
+      program_link = url_base + "#/program-details?programId=#{id}&from=%2Fprogram-search"
+      browser.goto(program_link)
+      browser.refresh
+      file = browser.a(text: "Review the Program Standards").href
+      file_path = file.gsub("https://", "")
+
+      begin
+        standards_import = StandardsImport.where(
+          name: file
+        ).first_or_create!(
+          notes: "From Scraper::WashingtonJob: #{program_link}"
+        )
+
+        standards_import.files.attach(io: URI.open("https://#{file_path}"), filename: File.basename(file))
+      rescue OpenURI::HTTPError
+        next
+      end
+    end
+    browser.close
   end
 end
