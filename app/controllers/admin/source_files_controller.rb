b09be617d41adfd6d5c220d108d89c10f6dbf4b1
Fernando Perales <fer@thoughtbot.com>
2023-03-22 09:09:59 -0600

    WIP

--- app/controllers/admin/source_files_controller.rb
+++ app/controllers/admin/source_files_controller.rb
@@ -7,18 +7,5 @@ module Admin
     def after_resource_updated_path(resource)
       admin_source_files_path
     end
-
-    private
-
-    def accessible_action?(target, action_name)
-      debugger
-      # super ||
-      # target = target.to_sym if target.is_a?(String)
-      # target_class_or_class_name =
-        # target.is_a?(ActiveRecord::Base) ? target.class : target
-
-      # existing_action?(target_class_or_class_name, action_name) &&
-        # authorized_action?(target, action_name)
-    end
   end
 end
