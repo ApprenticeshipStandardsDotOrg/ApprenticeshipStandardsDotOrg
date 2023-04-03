ba438eaa806e4349005eae5860b9b3eb99ea0f3d
Fernando Perales <fer@thoughtbot.com>
2023-04-03 14:04:01 -0600

    Admin can view and edit all attributes for occupation standards

--- app/dashboards/occupation_standard_dashboard.rb
+++ app/dashboards/occupation_standard_dashboard.rb
@@ -2,31 +2,32 @@ require "administrate/base_dashboard"
 
 class OccupationStandardDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
-    id: Field::String,
     apprenticeship_to_journeyworker_ratio: Field::String,
+    created_at: Field::DateTime.with_options(format: :short),
     data_imports: Field::HasMany,
     existing_title: Field::String,
+    id: Field::String,
+    national_standard_type: EnumField,
     occupation: Field::BelongsTo,
-    occupation_type: EnumField,
     ojt_hours_max: Field::Number,
     ojt_hours_min: Field::Number,
+    ojt_type: EnumField,
     onet_code: Field::String,
     organization: Field::BelongsTo,
     probationary_period_months: Field::Number,
     rapids_code: Field::String,
-    registration_agency: Field::BelongsTo,
+    registration_agency: Field::BelongsTo.with_options(scope: -> { RegistrationAgency.includes(:state) }),
     related_instructions: Field::HasMany,
     rsi_hours_max: Field::Number,
     rsi_hours_min: Field::Number,
+    source_file: Field::String,
     status: EnumField,
     term_months: Field::Number,
     title: Field::String,
-    url: Field::String,
+    updated_at: Field::DateTime.with_options(format: :short),
+    url: Field::Url,
     wage_steps: Field::HasMany,
-    work_processes: Field::HasMany,
-    source_file: Field::String,
-    created_at: Field::DateTime.with_options(format: :short),
-    updated_at: Field::DateTime.with_options(format: :short)
+    work_processes: Field::HasMany
   }.freeze
 
   COLLECTION_ATTRIBUTES = %i[
@@ -38,14 +39,29 @@ class OccupationStandardDashboard < Administrate::BaseDashboard
   ].freeze
 
   SHOW_PAGE_ATTRIBUTES = %i[
-    status
-    occupation
-    url
-    registration_agency
-    rapids_code
+
+    title
     onet_code
+    rapids_code
+    term_months
+    url
+    status
     created_at
     updated_at
+
+    apprenticeship_to_journeyworker_ratio
+    existing_title
+    occupation
+    national_standard_type
+    ojt_type
+    ojt_hours_max
+    ojt_hours_min
+    organization
+    probationary_period_months
+    registration_agency
+    rsi_hours_max
+    rsi_hours_min
+
     data_imports
     work_processes
     related_instructions
@@ -56,7 +72,22 @@ class OccupationStandardDashboard < Administrate::BaseDashboard
     title
     onet_code
     rapids_code
+    term_months
+    url
     status
+
+    apprenticeship_to_journeyworker_ratio
+    existing_title
+    occupation
+    national_standard_type
+    ojt_type
+    ojt_hours_max
+    ojt_hours_min
+    organization
+    probationary_period_months
+    registration_agency
+    rsi_hours_max
+    rsi_hours_min
   ].freeze
 
   COLLECTION_FILTERS = {}.freeze
