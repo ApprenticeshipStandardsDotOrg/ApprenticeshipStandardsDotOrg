93e7fd17ef138c4f7c59985a6654fe126dd37f27
Fernando Perales <fer@thoughtbot.com>
2023-04-04 07:26:34 -0600

    Admin can view and edit all attributes for occupation standards (#113)
    * Admin can view and edit all attributes for occupation standards
    
    * Add specs

--- app/dashboards/registration_agency_dashboard.rb
+++ app/dashboards/registration_agency_dashboard.rb
@@ -4,4 +4,8 @@ class RegistrationAgencyDashboard < Administrate::BaseDashboard
   def display_resource(registration_agency)
     registration_agency.to_s
   end
+
+  def collection_includes
+    super + [registration_agency: :state]
+  end
 end
