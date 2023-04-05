f4442b40e7640401f9c546a642d8ae0dc1d592b3
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-04-05 10:41:53 -0700

    Do not need custom create method for api_keys controller

---
+++ spec/system/admin/users/show_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe "admin/users/show" do
+  it "can create an api token", :admin do
+    admin = create(:admin)
+    user = create(:user)
+
+    login_as admin
+    visit admin_user_path(user)
+
+    expect{
+      click_on "Create API key"
+    }.to change(APIKey, :count).by(1)
+
+    expect(page).to have_text "API key was successfully created."
+    api_key = APIKey.last
+    expect(api_key.user).to eq user
+  end
+end
