27f82b82635e772591ac76fc7ffea8fee6d3c17d
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-04-17 15:23:04 -0700

    Display real data on public occupation standards page (#117)
    Co-authored-by: Fatima Burke <fatimaburke@fatimaswordmbp.mynetworksettings.com>

--- spec/system/occupation_standards/index_spec.rb
+++ spec/system/occupation_standards/index_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.describe "occupation_standards/index" do
   it "displays titles" do
-    pending("modifying homepage to display actual data")
     create(:occupation_standard, title: "Mechanic")
     create(:occupation_standard, title: "Pipe Fitter")
 
