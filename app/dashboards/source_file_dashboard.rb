e046c0107b8609816c8b89021ce4ecae4e1a1241
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-04-18 10:36:54 -0700

    Display more fields for source files (#127)

--- app/dashboards/source_file_dashboard.rb
+++ app/dashboards/source_file_dashboard.rb
@@ -6,6 +6,8 @@ class SourceFileDashboard < Administrate::BaseDashboard
     active_storage_attachment: Field::BelongsTo,
     data_imports: HasManyDataImportsField,
     metadata: Field::JSONB,
+    notes: Field::String.with_options(searchable: false),
+    organization: Field::String.with_options(searchable: false),
     status: EnumField,
     url: Field::Url.with_options(searchable: false),
     created_at: Field::DateTime,
@@ -14,6 +16,7 @@ class SourceFileDashboard < Administrate::BaseDashboard
 
   COLLECTION_ATTRIBUTES = %i[
     created_at
+    organization
     active_storage_attachment
     status
   ].freeze
@@ -21,8 +24,12 @@ class SourceFileDashboard < Administrate::BaseDashboard
   SHOW_PAGE_ATTRIBUTES = %i[
     data_imports
     metadata
+    organization
     active_storage_attachment
     url
+    notes
+    created_at
+    updated_at
   ].freeze
 
   FORM_ATTRIBUTES = %i[
