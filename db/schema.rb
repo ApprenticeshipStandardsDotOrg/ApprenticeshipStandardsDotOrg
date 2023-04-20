fe17e6acfea31735d6889f740a79ac03fb2f9c91
Fatima Burke <fatimaburke@fatimaswordmbp.mynetworksettings.com>
2023-04-20 09:58:34 -0400

    footer partial created

--- db/schema.rb
+++ db/schema.rb
@@ -140,7 +140,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_144051) do
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "version"
     t.index ["code"], name: "unique_code", unique: true
   end
 
