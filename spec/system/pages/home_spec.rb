86ce5190f4d55d49db6a8c15bbe0e5f93cc8309f
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-05-01 09:54:58 -0700

    Hook up search on Home page -- needs styling help

---
+++ spec/system/pages/home_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe "pages/home" do
+  it "filters occupations based on search term" do
+    mechanic = create(:occupation_standard, title: "Mechanic")
+    pipe_fitter = create(:occupation_standard, title: "Pipe Fitter")
+
+    visit home_page_path
+
+    fill_in "q", with: "Mechanic"
+
+    find("#search").click
+
+    expect(page).to have_current_path(occupation_standards_path(q: "Mechanic"))
+    expect(page).to have_text "Showing Results for Mechanic"
+    expect(page).to have_field("q", with: "Mechanic")
+
+    expect(page).to have_link "Mechanic", href: occupation_standard_path(mechanic)
+    expect(page).to_not have_link "Pipe Fitter", href: occupation_standard_path(pipe_fitter)
+  end
+end
