b3001775c1aaba9a65620ec64bcbfa6edc23d04b
Jennifer Lu <nejul33@gmail.com>
2023-05-02 14:15:59 -0400

    Add occupation standard file for no rapids code and update spec

--- spec/services/import_occupation_standard_details_spec.rb
+++ spec/services/import_occupation_standard_details_spec.rb
@@ -19,7 +19,8 @@ RSpec.describe ImportOccupationStandardDetails do
         _ca_saa = create(:registration_agency, state: ca, agency_type: :saa)
         onet = create(:onet, code: "13-1071.01")
 
-        occupation = create(:occupation, onet: onet)
+        occupation1 = create(:occupation, rapids_code: "0157")
+        _occupation2 = create(:occupation, onet: onet)
 
         data_import = create(:data_import, :pending)
 
@@ -29,7 +30,7 @@ RSpec.describe ImportOccupationStandardDetails do
 
         os = OccupationStandard.last
         expect(os.data_import).to eq data_import
-        expect(os.occupation).to eq occupation
+        expect(os.occupation).to eq occupation1
         expect(os.registration_agency).to eq ca_oa
         expect(os.title).to eq "HUMAN RESOURCE SPECIALIST"
         expect(os.existing_title).to eq "Career Development Technician"
@@ -37,7 +38,7 @@ RSpec.describe ImportOccupationStandardDetails do
         expect(os).to be_competency_based
         expect(os.probationary_period_months).to eq 3
         expect(os.onet_code).to eq "13-1071.01"
-        expect(os.rapids_code).to eq ""
+        expect(os.rapids_code).to eq "0157"
         expect(os.apprenticeship_to_journeyworker_ratio).to eq "5:1"
         expect(os.organization_title).to eq "Hardy Corporation"
         expect(os.ojt_hours_min).to be_nil
@@ -50,8 +51,8 @@ RSpec.describe ImportOccupationStandardDetails do
         ca = create(:state, abbreviation: "CA")
         ca_oa = create(:registration_agency, state: ca, agency_type: :oa)
 
-        onet = create(:onet, code: "13-1071.01")
-        occupation = create(:occupation, onet: onet)
+        create(:onet, code: "13-1071.01")
+        occupation = create(:occupation, rapids_code: "0157")
 
         data_import = create(:data_import, :hybrid, :pending)
 
@@ -69,13 +70,45 @@ RSpec.describe ImportOccupationStandardDetails do
         expect(os).to be_competency_based
         expect(os.probationary_period_months).to eq 3
         expect(os.onet_code).to eq "13-1071.01"
-        expect(os.rapids_code).to eq ""
+        expect(os.rapids_code).to eq "0157"
         expect(os.apprenticeship_to_journeyworker_ratio).to eq "5:1"
         expect(os.organization_title).to eq "Hardy Corporation"
         expect(os.ojt_hours_min).to be_nil
         expect(os.ojt_hours_max).to be_nil
         expect(os.rsi_hours_min).to be_nil
         expect(os.rsi_hours_max).to be_nil
+      end
+
+      it "creates an occupation standards record when there is no RAPIDS code" do
+        ca = create(:state, abbreviation: "CA")
+        ca_oa = create(:registration_agency, state: ca, agency_type: :oa)
+
+        onet = create(:onet, code: "31-1071.01")
+        occupation = create(:occupation, onet: onet)
+
+        data_import = create(:data_import, :no_rapids, :pending)
+
+        expect {
+          described_class.new(data_import).call
+        }.to change(OccupationStandard, :count).by(1)
+
+        os = OccupationStandard.last
+        expect(os.data_import).to eq data_import
+        expect(os.occupation).to eq occupation
+        expect(os.registration_agency).to eq ca_oa
+        expect(os.title).to eq "HUMAN RESOURCE MANAGER"
+        expect(os.existing_title).to be_nil
+        expect(os.term_months).to eq 12
+        expect(os).to be_time_based
+        expect(os.probationary_period_months).to eq 7
+        expect(os.onet_code).to eq "31-1071.01"
+        expect(os.rapids_code).to be_nil
+        expect(os.apprenticeship_to_journeyworker_ratio).to eq "5:1"
+        expect(os.organization_title).to eq "Hardy Corporation"
+        expect(os.ojt_hours_min).to be_nil
+        expect(os.ojt_hours_max).to eq 200
+        expect(os.rsi_hours_min).to be_nil
+        expect(os.rsi_hours_max).to eq 100
       end
     end
 
@@ -84,8 +117,8 @@ RSpec.describe ImportOccupationStandardDetails do
         ca = create(:state, abbreviation: "CA")
         ca_oa = create(:registration_agency, state: ca, agency_type: :oa)
 
-        onet = create(:onet, code: "13-1071.01")
-        occupation = create(:occupation, onet: onet)
+        create(:onet, code: "13-1071.01")
+        occupation = create(:occupation, rapids_code: "0157")
 
         data_import = create(:data_import)
         os = data_import.occupation_standard
@@ -104,7 +137,7 @@ RSpec.describe ImportOccupationStandardDetails do
         expect(os).to be_competency_based
         expect(os.probationary_period_months).to eq 3
         expect(os.onet_code).to eq "13-1071.01"
-        expect(os.rapids_code).to eq ""
+        expect(os.rapids_code).to eq "0157"
         expect(os.apprenticeship_to_journeyworker_ratio).to eq "5:1"
         expect(os.organization_title).to eq "Hardy Corporation"
         expect(os.ojt_hours_min).to be_nil
