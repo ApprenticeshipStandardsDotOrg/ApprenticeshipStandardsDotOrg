0a9fd13820fd7fdc0598724004190582d6661e91
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-05-02 16:35:12 -0700

    Slightly better request request spec

--- spec/requests/national_standards_spec.rb
+++ spec/requests/national_standards_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "NationalStandards", type: :request do
   describe "GET /index" do
     context "when guest" do
       it "returns http success" do
-        create_pair(:occupation_standard)
+        create_pair(:occupation_standard, :program_standard)
 
         get national_standards_path
 
