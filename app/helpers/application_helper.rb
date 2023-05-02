e01ce721f1ac13ddc4a508442daf40f217274b34
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-05-02 16:19:27 -0700

    Fix search on home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def search_form_url
+    target = controller_name == "pages" ? "occupation_standards" : controller_name
+    url_for(controller: target, action: "index")
+  end
 end
