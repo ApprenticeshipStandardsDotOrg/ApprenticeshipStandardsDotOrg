26a7284c35354e1f3466da23412df33fd21ae25c
Fernando Perales <fer@thoughtbot.com>
2023-05-03 11:25:45 -0600

    WIP

--- app/helpers/work_processes_helper.rb
+++ app/helpers/work_processes_helper.rb
@@ -1,5 +1,22 @@
 module WorkProcessesHelper
-  def hours_range(work_process)
-    [work_process.minimum_hours, work_process.maximum_hours].compact.uniq.join("-")
+  def hours_range(hours, fallback)
+    hours = [hours, fallback].compact.first
+    number_to_human(
+      hours,
+      format: '%n%u',
+      precision: 2,
+      units:
+      {
+        thousand: 'K',
+        million: 'M',
+        billion: 'B'
+      }
+    )
+  end
+
+  def hours_range_for_occupation(occupation_standard)
+    minimum_hours = occupation_standard.work_processes.sum(:minimum_hours)
+    maximum_hours = occupation_standard.work_processes.sum(:maximum_hours)
+    hours_range(maximum_hours, minimum_hours)
   end
 end
