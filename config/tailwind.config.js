2c7d4901da9eae587df1e599acb656e4b39bb74b
Fatima Burke <fatima@lurmarketing.com>
2023-05-03 15:29:02 -0400

    Add Filters to Search Form (#150)

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
+    './node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {
@@ -118,5 +119,6 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
+    require('flowbite/plugin'),
   ]
 }
