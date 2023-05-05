538404c80968ecba6f349414bcb4eb0186b89952
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-05-05 13:53:35 -0700

    Home page has featured subheader (#171)

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 RSpec.describe "Pages", type: :request do
   describe "GET /home" do
     it "returns http success" do
+      allow(State).to receive(:find_by).and_return(build_stubbed(:state))
+
       get home_page_path
 
       expect(response).to be_successful
