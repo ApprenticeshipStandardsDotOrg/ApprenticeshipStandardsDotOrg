8f0912fa79fd74088c11cea69a8903b9adc3b3d4
Fernando Perales <fer@thoughtbot.com>
2023-05-05 09:41:24 -0600

    Populate related work tab with real data

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,4 +5,8 @@ class Organization < ApplicationRecord
   has_many :occupation_standards
 
   validates :title, presence: true, uniqueness: true
+
+  def self.related_instructions_organizations(occupation_standard)
+    where(id: occupation_standard.related_instructions.pluck(:organization_id)).distinct
+  end
 end
