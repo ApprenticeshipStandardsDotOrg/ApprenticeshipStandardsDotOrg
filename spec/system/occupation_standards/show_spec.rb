38362687db2991919a0e7cd0915914954c88308a
Fernando Perales <fer@thoughtbot.com>
2023-05-11 08:01:12 -0600

    Remove HTML tags from competency title (#182)
    * Remove HTML tags from competency title
    
    * Add spec

--- spec/system/occupation_standards/show_spec.rb
+++ spec/system/occupation_standards/show_spec.rb
@@ -2,11 +2,28 @@ require "rails_helper"
 
 RSpec.describe "occupation_standards/show" do
   it "displays title" do
-    data_import = create(:data_import)
-    occupation_standard = data_import.occupation_standard
+    occupation_standard = create(:occupation_standard, :with_data_import)
 
     visit occupation_standard_path(occupation_standard)
 
     expect(page).to have_content occupation_standard.title
+  end
+
+  it "removes HTML tags from competencies title" do
+    occupation_standard = create(:occupation_standard, :with_data_import)
+    work_process = create(
+      :work_process,
+      occupation_standard: occupation_standard,
+      title: "Sample Work Process"
+    )
+    create(
+      :competency,
+      work_process: work_process,
+      title: "<html>Title with<b> HTML</b><html>"
+    )
+
+    visit occupation_standard_path(occupation_standard)
+
+    expect(page).to have_content "Title with HTML"
   end
 end
