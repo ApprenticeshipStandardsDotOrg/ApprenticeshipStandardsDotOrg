336d8ded373b9b9e1dd61a4ee1e783082947bfd3
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-05-16 15:09:34 -0700

    Change checkBox to checkbox in search controller

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,10 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "textField", "checkBox" ]
+  static targets = [ "textField", "checkbox" ]
 
   clearForm() {
     this.textFieldTargets.forEach(input => input.value = "")
-    this.checkBoxTargets.forEach(input => input.checked = false)
+    this.checkboxTargets.forEach(input => input.checked = false)
   }
 }
