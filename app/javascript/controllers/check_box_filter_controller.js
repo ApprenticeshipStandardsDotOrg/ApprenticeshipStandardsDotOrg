280151d63cdd3e4701801e46f593f95c0e6ab17e
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-05-16 15:07:05 -0700

    Remove unnecessary Stimulus value

--- app/javascript/controllers/check_box_filter_controller.js
+++ app/javascript/controllers/check_box_filter_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "input", "count", "countWrapper" ]
   static classes = [ "displayable" ]
-  static values = { count: Number }
 
   connect() {
     this.updateCount()
@@ -11,13 +10,12 @@ export default class extends Controller {
 
   updateCount() {
     const length = this.inputTargets.filter(checkbox => checkbox.checked).length
-    this.countValue = length
-    this.setCountValue()
+    this.setCountValue(length)
   }
 
-  setCountValue() {
-    this.countTarget.innerHTML = this.countValue
-    if (this.countValue > 0) {
+  setCountValue(length) {
+    this.countTarget.innerHTML = length
+    if (length > 0) {
       this.countWrapperTarget.classList.add(this.displayableClass)
     } else {
       this.countWrapperTarget.classList.remove(this.displayableClass)
