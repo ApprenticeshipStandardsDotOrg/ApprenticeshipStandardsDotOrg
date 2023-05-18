c3284b64cd36dd971b207d7d928204cb5238f60f
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-05-18 15:18:26 -0700

    Minor refactor and unhide Industry nav link

--- app/controllers/industries_controller.rb
+++ app/controllers/industries_controller.rb
@@ -1,7 +1,6 @@
 class IndustriesController < ApplicationController
   def index
-    @industries = Industry.current.order(:name)
-    industry_info = @industries.pluck(:name, :id, :prefix).sort
+    industry_info = Industry.current.order(:name).pluck(:name, :id, :prefix).sort
     @names_by_letter = industry_info.group_by { |industry| industry[0][0] }
   end
 end
