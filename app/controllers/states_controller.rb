3734cf42e93583fd0c1ed4d02a2de3826998931a
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-05-18 11:47:57 -0700

    Move variables into controller

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,4 +1,6 @@
 class StatesController < ApplicationController
   def index
+    state_info = State.all.pluck(:name, :id)
+    @names_by_letter = state_info.group_by { |state| state[0][0] }
   end
 end
