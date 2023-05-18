e4b1ea3fa5378d14483497028b037dde8ef54758
Fatima Burke <fatimaburke@fatimaswordmbp.mynetworksettings.com>
2023-05-18 17:52:36 -0400

    browse by industry layout

--- app/controllers/industries_controller.rb
+++ app/controllers/industries_controller.rb
@@ -1,5 +1,7 @@
 class IndustriesController < ApplicationController
   def index
     @industries = Industry.current.order(:name)
+    industry_info = @industries.pluck(:name, :id, :prefix).sort
+    @names_by_letter = industry_info.group_by { |industry| industry[0][0] }
   end
 end
