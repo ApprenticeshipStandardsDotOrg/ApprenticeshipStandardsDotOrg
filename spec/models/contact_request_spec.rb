3b0139ff905b7a3b33600f938c6e2d2319a3b7b0
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-05-23 13:45:46 -0700

    Call mailer to notify admin when contact request is created

--- spec/models/contact_request_spec.rb
+++ spec/models/contact_request_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe ContactRequest, type: :model do
 
     expect(contact_request).to be_valid
   end
+
+  describe "#notify_admin" do
+    it "calls new_contact_request mailer on create but not update" do
+      contact = build(:contact_request)
+
+      mailer = double("mailer", deliver_later: nil)
+      expect(AdminMailer).to receive(:new_contact_request).and_return(mailer)
+      expect(mailer).to receive(:deliver_later)
+
+      contact.save!
+
+      expect(AdminMailer).to_not receive(:new_contact_request)
+
+      contact.update!(name: "Minnie Mouse")
+    end
+  end
 end
