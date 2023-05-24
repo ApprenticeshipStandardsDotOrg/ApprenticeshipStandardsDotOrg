15eb2f93ef32ce3b6fc642b1b5add9a78f3ce6d6
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-05-24 11:54:09 -0700

    Add system spec for converter for source_files#show page

--- spec/system/admin/source_files/show_spec.rb
+++ spec/system/admin/source_files/show_spec.rb
@@ -22,5 +22,30 @@ RSpec.describe "admin/source_files/show", :admin do
     expect(page).to have_link "Edit", href: edit_admin_source_file_data_import_path(source_file, data_import2)
 
     expect(page).to have_link "New data import", href: new_admin_source_file_data_import_path(source_file)
+#    expect(page).to_not have_button "Need support"
+  end
+
+  it "has Need support button viewable for converters" do
+    converter = create(:user, :converter)
+    source_file = create(:source_file)
+
+    occupation_standard1 = create(:occupation_standard, title: "Mechanic")
+    data_import1 = create(:data_import, source_file: source_file, occupation_standard: occupation_standard1)
+
+    data_import2 = create(:data_import, :hybrid, source_file: source_file, occupation_standard: nil)
+
+    login_as converter
+    visit admin_source_file_path(source_file)
+
+    expect(page).to have_content "Show #{source_file.filename}"
+
+    expect(page).to_not have_link "Edit", href: edit_admin_source_file_data_import_path(source_file, data_import1)
+    expect(page).to have_link "occupation-standards-template.xlsx", href: admin_source_file_data_import_path(source_file, data_import1)
+    expect(page).to_not have_link "Mechanic", href: admin_occupation_standard_path(occupation_standard1)
+
+    expect(page).to_not have_link "Edit", href: edit_admin_source_file_data_import_path(source_file, data_import2)
+
+    expect(page).to have_link "New data import", href: new_admin_source_file_data_import_path(source_file)
+#    expect(page).to have_button "Need support"
   end
 end
