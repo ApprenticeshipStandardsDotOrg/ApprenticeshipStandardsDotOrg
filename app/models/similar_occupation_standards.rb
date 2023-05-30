8d1f6027b2a21a94c65640b603f76bdee50c1cb1
sarahraqueld <sarahraqueld@gmail.com>
2023-05-30 09:57:11 -0300

    Refactor

--- app/models/similar_occupation_standards.rb
+++ app/models/similar_occupation_standards.rb
@@ -32,7 +32,10 @@ class SimilarOccupationStandards
           min_term_freq: 1,
           analyzer: "snowball"
         }
-      }
+      },
+      sort: [
+        {_score: {order: "desc"}}
+      ]
     }
   end
 end
