b3997b5d7ee88235d17fb8a889f4fa234f286e19
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-06-14 11:38:10 -0700

    Add more searching capabilities for Admin occupation standards (#293)
    * Allow admin searching of occupation standard by registration agency
    
    * Allow searching occupation_standards by occupation
    
    * Add RAPIDS code to index page display
    
    * When searching standard by rapids code, also search on occupation's rapid code

--- app/controllers/admin/occupation_standards_controller.rb
+++ app/controllers/admin/occupation_standards_controller.rb
@@ -3,5 +3,38 @@ module Admin
     def scoped_resource
       OccupationStandard.includes(occupation: :onet, registration_agency: :state)
     end
+
+    def filter_resources(resources, search_term:)
+      Administrate::OccupationStandardSearch.new(
+        resources,
+        dashboard,
+        search_term
+      ).run
+    end
+  end
+end
+
+module Administrate
+  class OccupationStandardSearch < Search
+    private
+
+    def query_template
+      super
+        .split(" OR ")
+        .push("LOWER(states.name) LIKE ?")
+        .push("LOWER(occupations.title) LIKE ?")
+        .push("occupations.rapids_code LIKE ?")
+        .join(" OR ")
+    end
+
+    def query_values
+      values = super
+      term = values.first
+      values + [term, term, term]
+    end
+
+    def search_results(resources)
+      super.left_joins(registration_agency: :state).left_joins(:occupation)
+    end
   end
 end
