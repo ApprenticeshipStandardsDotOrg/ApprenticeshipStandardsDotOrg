8b76b863fefc431ffd854f00eaf30b6c5d44d0ff
Jennifer Lu <nejul33@gmail.com>
2023-06-15 10:08:50 -0400

    Add number of occupations next to states

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,5 +2,6 @@ class StatesController < ApplicationController
   def index
     state_info = State.order(:name).pluck(:name, :id)
     @names_by_letter = state_info.group_by { |state| state[0][0] }
+    @occupation_standards_count_by_state = OccupationStandard.joins(registration_agency: :state).group('states.id').count
   end
 end
