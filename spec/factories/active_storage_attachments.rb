61bb1b43ee76df4a7d687ad11bc3e89a8bb86997
sarahraqueld <sarahraqueld@gmail.com>
2023-06-16 18:19:52 -0300

    Undo unnecessary changes

--- spec/factories/active_storage_attachments.rb
+++ spec/factories/active_storage_attachments.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :active_storage_attachment, class: "ActiveStorage::Attachment" do
-    name { "abc999" }
+    name { "abc" }
     association :record, factory: :standards_import
     association :blob, factory: :active_storage_blob
     content_type { "image/jpeg" }
--- spec/factories/active_storage_blobs.rb
+++ spec/factories/active_storage_blobs.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :active_storage_blob, class: "ActiveStorage::Blob", aliases: [:blob] do
-    sequence(:key) { |n| "abc#{n}999" }
+    sequence(:key) { |n| "abc#{n}" }
     filename { "abc" }
     service_name { :local }
     byte_size { 1 }
