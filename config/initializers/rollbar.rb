27f205553b461c492c06e95801193b66e3ddd572
sarahraqueld <sarahraqueld@gmail.com>
2023-06-19 11:29:08 -0300

    Enable rollbar in staging

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -4,7 +4,7 @@ Rollbar.configure do |config|
 
   config.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
 
-  unless Rails.env.production?
+  unless Rails.env.production? || Rails.env.staging?
     config.enabled = false
   end
 
