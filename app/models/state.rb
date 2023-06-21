a8733d042c736d0dc95893ec7a92b8118f035699
Jennifer Lu <nejul33@gmail.com>
2023-06-21 12:49:17 -0400

    Move state count to model and add unit test

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,2 +1,8 @@
 class State < ApplicationRecord
+  has_many :registration_agencies
+  has_many :occupation_standards, through: :registration_agencies
+
+  def occupation_standards_count
+    self.occupation_standards.count
+  end
 end
--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe State, type: :model do
 
     expect(state).to be_valid
   end
+
+  describe "#occupation_standards_count" do
+    it "returns an occupation standards count by state" do
+      state = create(:state)
+      registration_agency = create(:registration_agency, state: state)
+      occupation_standards = create_list(:occupation_standard, 3, registration_agency: registration_agency)
+
+      expect(state.occupation_standards_count).to eq(3)
+    end
+  end
 end
