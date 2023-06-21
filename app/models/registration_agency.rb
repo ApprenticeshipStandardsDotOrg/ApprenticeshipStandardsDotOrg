0c4cdd4321eeff986e96878bf283419291c52b6c
Jen Lu <58493319+jenlu33@users.noreply.github.com>
2023-06-21 13:35:44 -0400

    Add number of occupations next to states (#297)
    * Add number of occupations next to states
    
    ---------
    
    Co-authored-by: Sarah Lima <sarahraqueld@gmail.com>

--- app/models/registration_agency.rb
+++ app/models/registration_agency.rb
@@ -3,6 +3,7 @@ class RegistrationAgency < ApplicationRecord
   validates :state, uniqueness: {scope: :agency_type}
 
   belongs_to :state, optional: true
+  has_many :occupation_standards
 
   enum agency_type: [:oa, :saa]
 
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,2 +1,8 @@
 class State < ApplicationRecord
+  has_many :registration_agencies
+  has_many :occupation_standards, through: :registration_agencies
+
+  def occupation_standards_count
+    occupation_standards.count
+  end
 end
