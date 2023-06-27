305c06d608eebaa69effdcbb391114a973bf0e6d
sarahraqueld <sarahraqueld@gmail.com>
2023-06-27 10:59:14 -0300

    Refactor

--- app/queries/popular_onet_codes_query.rb
+++ app/queries/popular_onet_codes_query.rb
@@ -1,12 +1,7 @@
 class PopularOnetCodesQuery
   def self.run(limit: 4)
-    sql = "SELECT COUNT(onet_code), onet_code
-    FROM occupation_Standards
-    WHERE onet_code IS NOT NULL
-    GROUP BY onet_code
-    ORDER BY COUNT(onet_code) DESC
-    LIMIT #{limit}"
-
-    OccupationStandard.find_by_sql(sql).pluck(:onet_code)
+    OccupationStandard.where.not(onet_code: nil)
+      .group(:onet_code)
+      .order("COUNT(onet_code) DESC").limit(limit)
   end
 end
