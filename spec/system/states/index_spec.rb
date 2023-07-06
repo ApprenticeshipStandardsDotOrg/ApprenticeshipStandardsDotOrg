9ee21cbcb24410eb579eae84c9e8999881a23e01
Jen Lu <58493319+jenlu33@users.noreply.github.com>
2023-07-06 13:51:58 -0400

    Remove zero state count (#331)
    * Only show state count if not zero

--- spec/system/states/index_spec.rb
+++ spec/system/states/index_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe "states/index" do
   it "lists states with link to search by state and shows occupation standards count" do
     wa = create(:state, name: "Washington", abbreviation: "WA")
     al = create(:state, name: "Alabama", abbreviation: "AL")
+    va = create(:state, name: "Virginia", abbreviation: "VA")
 
     wa_reg_agency = create(:registration_agency, state: wa)
     al_reg_agency = create(:registration_agency, state: al)
+    create(:registration_agency, state: va)
 
     mechanic = create(:occupation_standard, :with_work_processes, :with_data_import, title: "Mechanic", registration_agency: wa_reg_agency)
     create_list(:occupation_standard, 3, :with_work_processes, :with_data_import, :program_standard, registration_agency: al_reg_agency)
@@ -15,6 +17,7 @@ RSpec.describe "states/index" do
 
     expect(page).to have_link "Washington 1", href: occupation_standards_path(state_id: wa.id)
     expect(page).to have_link "Alabama 3", href: occupation_standards_path(state_id: al.id)
+    expect(page).to have_link "Virginia", href: occupation_standards_path(state_id: va.id)
 
     click_on "Washington 1"
 
