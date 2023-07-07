e2cc2e0958098ef61180f89bfa2ec17cfb9043cb
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-07-07 14:57:55 -0700

    Move recaptcha stubs out to common helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,7 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Warden::Test::Helpers
+  config.include Helpers::WebmockHelpers
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
--- spec/requests/contact_requests_spec.rb
+++ spec/requests/contact_requests_spec.rb
@@ -95,37 +95,4 @@ RSpec.describe "ContactRequest", type: :request do
       end
     end
   end
-
-  def stub_recaptcha_high_score
-    stub_request(:post, /google.*recaptcha/)
-      .to_return(status: 200, body: {
-        "success": true,
-        "challenge_ts": "2023-07-07T19:40:09Z",
-        "hostname": "apprenticeshipstandards.org",
-        "score": 0.9,
-        "action": "submit"
-      }.to_json, headers: {})
-  end
-
-  def stub_recaptcha_low_score
-    stub_request(:post, /google.*recaptcha/)
-      .to_return(status: 200, body: {
-        "success": true,
-        "challenge_ts": "2023-07-07T19:40:09Z",
-        "hostname": "apprenticeshipstandards.org",
-        "score": 0.2,
-        "action": "submit"
-      }.to_json, headers: {})
-  end
-
-  def stub_recaptcha_failure
-    stub_request(:post, /google.*recaptcha/)
-      .to_return(status: 200, body: {
-        "success": false,
-        "challenge_ts": "2023-07-07T19:40:09Z",
-        "hostname": "apprenticeshipstandards.org",
-        "error_codes": ["missing-input-secret"],
-        "action": "submit"
-      }.to_json, headers: {})
-  end
 end
