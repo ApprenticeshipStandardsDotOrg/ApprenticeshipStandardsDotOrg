aace42d30314d581fc93f47b849329d3316f6339
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-07-07 14:17:09 -0700

    Add Google reCAPTCHA to contact form

--- app/controllers/contact_requests_controller.rb
+++ app/controllers/contact_requests_controller.rb
@@ -1,4 +1,8 @@
 class ContactRequestsController < ApplicationController
+  before_action :verify_recaptcha, only: :create
+
+  VALID_RECAPTCHA_SCORE = 0.5
+
   def new
     @contact_request = ContactRequest.new
   end
@@ -23,5 +27,26 @@ class ContactRequestsController < ApplicationController
       :email,
       :message
     )
+  end
+
+  def verify_recaptcha
+    uri = URI("https://www.google.com/recaptcha/api/siteverify")
+    resp = Net::HTTP.post_form(
+      uri,
+      secret: ENV["GOOGLE_RECAPTCHA_SECRET_KEY"],
+      response: params["g-recaptcha-response"],
+      remoteip: request.remote_ip
+    )
+    parsed_json = JSON.parse(resp.body)
+    success = parsed_json["success"]
+    score = parsed_json["score"].to_f
+
+    return if success && score >= VALID_RECAPTCHA_SCORE
+
+    unless success
+      Rails.error.report("Error with Google reCAPTCHA", context: parsed_json, handled: true)
+    end
+
+    redirect_to guest_root_path
   end
 end
--- spec/requests/contact_requests_spec.rb
+++ spec/requests/contact_requests_spec.rb
@@ -10,8 +10,10 @@ RSpec.describe "ContactRequest", type: :request do
   end
 
   describe "POST /create" do
-    context "with valid params" do
+    context "with valid params and decent Google recaptcha score" do
       it "creates a contact_request record" do
+        stub_recaptcha_high_score
+
         expect {
           post contact_requests_path, params: {
             contact_request: {
@@ -32,10 +34,54 @@ RSpec.describe "ContactRequest", type: :request do
       end
     end
 
-    context "with invalid params" do
-      it "does not creates a contact_request record" do
+    context "with valid params and poor Google recaptcha score" do
+      it "does not create a contact_request record" do
+        stub_recaptcha_low_score
+
         expect {
           post contact_requests_path, params: {
+            "g-captcha-response": "foobar",
+            contact_request: {
+              name: "Mickey",
+              email: "mickey@mouse.com",
+              organization: "Disney",
+              message: "We are happy"
+            }
+          }
+        }.to_not change(ContactRequest, :count)
+
+        expect(response).to redirect_to guest_root_path
+      end
+    end
+
+    context "with valid params and unsuccessful recaptcha score" do
+      it "does not create a contact_request record and reports error" do
+        stub_recaptcha_failure
+
+        expect_any_instance_of(ErrorSubscriber).to receive(:report).and_call_original
+        expect {
+          post contact_requests_path, params: {
+            "g-captcha-response": "foobar",
+            contact_request: {
+              name: "Mickey",
+              email: "mickey@mouse.com",
+              organization: "Disney",
+              message: "We are happy"
+            }
+          }
+        }.to_not change(ContactRequest, :count)
+
+        expect(response).to redirect_to guest_root_path
+      end
+    end
+
+    context "with invalid params" do
+      it "does not creates a contact_request record" do
+        stub_recaptcha_high_score
+
+        expect {
+          post contact_requests_path, params: {
+            "g-captcha-response": "foobar",
             contact_request: {
               name: "Mickey",
               email: "",
@@ -48,5 +94,38 @@ RSpec.describe "ContactRequest", type: :request do
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
+  end
+
+  def stub_recaptcha_high_score
+    stub_request(:post, /google.*recaptcha/)
+      .to_return(status: 200, body: {
+        "success": true,
+        "challenge_ts": "2023-07-07T19:40:09Z",
+        "hostname": "apprenticeshipstandards.org",
+        "score": 0.9,
+        "action": "submit"
+      }.to_json, headers: {})
+  end
+
+  def stub_recaptcha_low_score
+    stub_request(:post, /google.*recaptcha/)
+      .to_return(status: 200, body: {
+        "success": true,
+        "challenge_ts": "2023-07-07T19:40:09Z",
+        "hostname": "apprenticeshipstandards.org",
+        "score": 0.2,
+        "action": "submit"
+      }.to_json, headers: {})
+  end
+
+  def stub_recaptcha_failure
+    stub_request(:post, /google.*recaptcha/)
+      .to_return(status: 200, body: {
+        "success": false,
+        "challenge_ts": "2023-07-07T19:40:09Z",
+        "hostname": "apprenticeshipstandards.org",
+        "error_codes": ["missing-input-secret"],
+        "action": "submit"
+      }.to_json, headers: {})
   end
 end
