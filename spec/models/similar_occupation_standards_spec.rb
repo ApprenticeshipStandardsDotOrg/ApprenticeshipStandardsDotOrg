8d4556932d7aa0106250c6e678a5aeca470449ca
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-07-11 12:24:37 -0700

    Spec cleanup

--- spec/models/similar_occupation_standards_spec.rb
+++ spec/models/similar_occupation_standards_spec.rb
@@ -12,14 +12,22 @@ RSpec.describe SimilarOccupationStandards, type: :model do
       wp1a = create(:work_process, title: "Principles of Child Growth and Development", occupation_standard: os1)
       wp1b = create(:work_process, title: "Social and Emotional Development", occupation_standard: os1)
 
+      # Matches on title, state
       os4 = create(:occupation_standard, :hybrid, title: "Childcare worker", registration_agency: wa_reg_agency)
       wp4a = create(:work_process, title: "Vehicle Inspection", occupation_standard: os4)
       wp4b = create(:work_process, title: "Tracking and Management Systems", occupation_standard: os4)
 
+      # Matches on title, ojt_type
+      os6 = create(:occupation_standard, :time, title: "Childcare worker", registration_agency: al_reg_agency)
+      wp6a = create(:work_process, title: "Vehicle Inspection", occupation_standard: os4)
+      wp6b = create(:work_process, title: "Tracking and Management Systems", occupation_standard: os4)
+
+      # Matches on title, state, work process titles
       os2 = create(:occupation_standard, :hybrid, title: "Childcare worker", registration_agency: wa_reg_agency)
       wp2a = create(:work_process, title: "Principles of Child Growth and Development", occupation_standard: os2)
       wp2b = create(:work_process, title: "Social and Emotional Development", occupation_standard: os2)
 
+      # Matches on title, work process titles
       os3 = create(:occupation_standard, :hybrid, title: "Childcare worker", registration_agency: al_reg_agency)
       wp3a = create(:work_process, title: "Principles of Child Growth and Development", occupation_standard: os3)
       wp3b = create(:work_process, title: "Social and Emotional Development", occupation_standard: os3)
@@ -31,7 +39,7 @@ RSpec.describe SimilarOccupationStandards, type: :model do
       OccupationStandard.import
       OccupationStandard.__elasticsearch__.refresh_index!
 
-      expect(described_class.similar_to(os1.reload, true)).to eq [os2, os3, os4]
+      expect(described_class.similar_to(os1.reload, true)).to eq [os2, os3, os4, os6]
     end
   end
 end
