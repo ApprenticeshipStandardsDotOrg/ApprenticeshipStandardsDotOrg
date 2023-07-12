4f7fc1f4fd003b0a2ad977edb53b8c342eb8ba3c
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-07-12 16:44:28 -0700

    lint

--- spec/support/helpers/webmock_helpers.rb
+++ spec/support/helpers/webmock_helpers.rb
@@ -6,11 +6,11 @@ module Helpers
       stub_const "ENV", ENV.to_h.merge("ENABLE_RECAPTCHA" => "true")
       stub_request(:post, /google.*recaptcha/)
         .to_return(status: 200, body: {
-          "success": true,
-          "challenge_ts": "2023-07-07T19:40:09Z",
-          "hostname": "apprenticeshipstandards.org",
-          "score": 0.9,
-          "action": "submit"
+          success: true,
+          challenge_ts: "2023-07-07T19:40:09Z",
+          hostname: "apprenticeshipstandards.org",
+          score: 0.9,
+          action: "submit"
         }.to_json, headers: {})
     end
 
@@ -18,11 +18,11 @@ module Helpers
       stub_const "ENV", ENV.to_h.merge("ENABLE_RECAPTCHA" => "true")
       stub_request(:post, /google.*recaptcha/)
         .to_return(status: 200, body: {
-          "success": true,
-          "challenge_ts": "2023-07-07T19:40:09Z",
-          "hostname": "apprenticeshipstandards.org",
-          "score": 0.2,
-          "action": "submit"
+          success: true,
+          challenge_ts: "2023-07-07T19:40:09Z",
+          hostname: "apprenticeshipstandards.org",
+          score: 0.2,
+          action: "submit"
         }.to_json, headers: {})
     end
 
@@ -30,11 +30,11 @@ module Helpers
       stub_const "ENV", ENV.to_h.merge("ENABLE_RECAPTCHA" => "true")
       stub_request(:post, /google.*recaptcha/)
         .to_return(status: 200, body: {
-          "success": false,
-          "challenge_ts": "2023-07-07T19:40:09Z",
-          "hostname": "apprenticeshipstandards.org",
-          "error_codes": ["missing-input-secret"],
-          "action": "submit"
+          success: false,
+          challenge_ts: "2023-07-07T19:40:09Z",
+          hostname: "apprenticeshipstandards.org",
+          error_codes: ["missing-input-secret"],
+          action: "submit"
         }.to_json, headers: {})
     end
   end
