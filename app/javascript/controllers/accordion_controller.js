ef6ba8357b7de2b30c9cac722451662050d9717b
Crystal Williams-Brown <crystal_wb@thoughtbot.com>
2023-07-13 13:22:58 -0400

    setup stimulus controller

--- app/javascript/controllers/accordion_controller.js
+++ app/javascript/controllers/accordion_controller.js
@@ -1,11 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = [ "accordion", "expandButton" ]
 
-  changeVisibility(element_id) {
-       var panel = document.getElementById(element_id);
-       var panelButton = document.getElementById(`${element_id}-button`);
-       panel.classList.toggle("hidden")
-       panelButton.classList.toggle("hidden")
+  changeVisibility() {
+       this.accordionTarget.classList.toggle("hidden");
+       this.expandButtonTarget.classList.toggle("hidden");
    }
 }
