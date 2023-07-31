c2523949b06c6966ad23f2f5ca3b3b34b6fa5c68
Fernando Perales <fer@thoughtbot.com>
2023-07-31 12:53:48 -0600

    Fix linter

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,7 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join(
-  "node_modules/@foxitsoftware/foxit-pdf-sdk-for-web-library-full",
+  "node_modules/@foxitsoftware/foxit-pdf-sdk-for-web-library-full"
 )
 
 # Precompile additional assets.
