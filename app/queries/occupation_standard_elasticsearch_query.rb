5fb1f77f24acf945a9a8e78315c8c50c92403ea9
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-08-09 10:56:01 -0700

    WIP new elasticsearch query file

---
+++ app/queries/occupation_standard_elasticsearch_query.rb
@@ -0,0 +1,96 @@
+require "elasticsearch/dsl"
+
+class OccupationStandardElasticsearchQuery
+  include Elasticsearch::DSL
+
+  attr_reader :search_term_params, :debug
+
+  def initialize(search_term_params, debug = true)
+    @search_term_params = search_term_params
+    @debug = debug
+  end
+
+  def do_search
+    puts "search params"
+    puts search_term_params
+    definition = search do
+      query do
+        bool do
+          if search_term_params[:state_id].present?
+            filter do
+              term state_id: search_term_params[:state_id]
+            end
+          end
+          if search_term_params[:state].present?
+            filter do
+              term state: search_term_params[:state]
+            end
+          end
+          if search_term_params[:national_standard_type].present?
+            bool do
+              search_term_params[:national_standard_type].keys.each do |type|
+                should do
+                  match national_standard_type: {
+                    query: type
+                  }
+                end
+              end
+              minimum_should_match 1
+            end
+          end
+          if search_term_params[:ojt_type].present?
+            bool do
+              search_term_params[:ojt_type].keys.each do |type|
+                should do
+                  match ojt_type: {
+                    query: type
+                  }
+                end
+              end
+              minimum_should_match 1
+            end
+          end
+          if search_term_params[:q].present?
+            puts "here"
+            q = search_term_params[:q]
+            bool do
+              should do
+                match title: {
+                  query: q
+                }
+              end
+              should do
+                match rapids_code: {
+                  query: q
+                }
+              end
+              should do
+                match onet_code: {
+                  query: q
+                }
+              end
+              should do 
+                match industry_name: {
+                  query: q
+                }
+              end
+              minimum_should_match 1
+            end
+          end
+        end
+      end
+    end
+    puts "DEFINITION"
+    puts definition.to_hash
+    puts "DEFINITION END"
+    response = OccupationStandard.__elasticsearch__.search(definition)
+    if debug
+      puts "QUERY"
+      puts response.search.definition[:body][:query].to_json
+      puts "HITS: #{response.results.total}"
+      response.results.each do |result|
+        puts "#{result._id}: #{result._score}"
+      end
+    end
+  end
+end
