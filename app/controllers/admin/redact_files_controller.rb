d051d39986bdf7aa10c83b2d9db2a7a83240a359
Fernando Perales <fer@thoughtbot.com>
2023-08-14 09:43:57 -0600

    Allow admins to save redacted document

--- app/controllers/admin/redact_files_controller.rb
+++ app/controllers/admin/redact_files_controller.rb
@@ -6,5 +6,20 @@ module Admin
       # Use a custom layout
       render layout: "admin/pdf_editor"
     end
+
+    def create
+      @source_file = SourceFile.find(params[:source_file_id])
+      authorize :redact_file, :new?
+      if params[:redacted_file]
+        @source_file.data_imports.map(&:occupation_standard).each do |occupation_standard|
+          occupation_standard.redacted_document.attach(params[:redacted_file])
+        end
+        flash[:notice] = "Occupation Standards updated with redacted file"
+      else
+        flash[:alert] = "No changes"
+      end
+
+      redirect_to admin_source_file_path(@source_file)
+    end
   end
 end
