98514b5e05ff175286059b375c35b8f779f3d475
Jen Lu <58493319+jenlu33@users.noreply.github.com>
2023-08-17 14:05:56 -0400

    Exclude Occupation Standards with No Work Processes (#379)
    * Only find popular occupations with work processes
    
    * Only show industries with work processes and update spec

--- app/queries/popular_onet_codes_query.rb
+++ app/queries/popular_onet_codes_query.rb
@@ -1,9 +1,11 @@
 class PopularOnetCodesQuery
   LIMIT = 4
   def self.run(limit: LIMIT)
-    OccupationStandard.where.not(onet_code: nil)
+    OccupationStandard
+      .joins(:work_processes)
+      .where.not(onet_code: nil)
       .group(:onet_code)
-      .order("COUNT(onet_code) DESC")
+      .order(Arel.sql("COUNT(DISTINCT occupation_standards.id) DESC"))
       .limit(limit)
       .pluck(:onet_code)
   end
