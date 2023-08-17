21f600fcd6a4ae6264252dee21cdbce26ed33bfb
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-08-16 20:07:07 -0700

    Refactor with helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,17 @@ module ApplicationHelper
     target = (controller_name == "pages") ? "occupation_standards" : controller_name
     url_for(controller: target, action: "index")
   end
+
+  def national_standard_types_filter
+    @_filter ||= OccupationStandard.national_standard_types.transform_values{|v| v = 1}
+  end
+
+  def occupation_standards_page_active?
+    !current_page?(occupation_standards_path(national_standard_type: national_standard_types_filter), check_parameters: true) &&
+      current_page?(occupation_standards_path)
+  end
+
+  def national_page_active?
+    current_page?(occupation_standards_path(national_standard_type: national_standard_types_filter), check_parameters: true)
+  end
 end
