844c3a38d89aef32fc258bf2ef7d7ad9ceebe3b5
Jen Lu <58493319+jenlu33@users.noreply.github.com>
2023-08-23 14:22:57 -0400

    Clickable states (#390)
    * Create popular states method and make states clickable

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,8 +1,17 @@
 class State < ApplicationRecord
+  POPULAR_LIMIT = 4
+
   has_many :registration_agencies
   has_many :occupation_standards, through: :registration_agencies
 
+  def self.popular(limit: POPULAR_LIMIT)
+    State.joins(occupation_standards: :work_processes)
+      .group("states.id")
+      .order(Arel.sql("COUNT(DISTINCT occupation_standards.id) DESC"))
+      .limit(limit)
+  end
+
   def occupation_standards_count
-    (occupation_standards.count > 0) ? occupation_standards.count : ""
+    (occupation_standards.with_work_processes.count > 0) ? occupation_standards.with_work_processes.count : ""
   end
 end
