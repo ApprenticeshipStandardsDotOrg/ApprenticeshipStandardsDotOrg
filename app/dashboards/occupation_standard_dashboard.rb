d1915d581a7d4a2451024a03ddbef9f4903406e7
Jennifer Lu <nejul33@gmail.com>
2023-09-26 13:41:35 -0400

    Add related_job_titles to admin/occupation_standard show view

--- app/dashboards/occupation_standard_dashboard.rb
+++ app/dashboards/occupation_standard_dashboard.rb
@@ -18,6 +18,7 @@ class OccupationStandardDashboard < Administrate::BaseDashboard
     rapids_code: Field::String,
     registration_agency: Field::BelongsTo.with_options(scope: -> { RegistrationAgency.includes(:state) }),
     related_instructions: Field::HasMany,
+    related_job_titles: Field::String,
     rsi_hours_max: Field::Number,
     rsi_hours_min: Field::Number,
     source_file: Field::String.with_options(searchable: false),
@@ -68,6 +69,7 @@ class OccupationStandardDashboard < Administrate::BaseDashboard
     work_processes
     related_instructions
     wage_steps
+    related_job_titles
   ].freeze
 
   FORM_ATTRIBUTES = %i[
