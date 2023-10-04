99cb213b48e57743c3f5688e1d211937727d1ce5
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-10-04 09:43:22 -0700

    Add down method for migration adding new onets indexes

--- db/migrate/20231003233735_add_version_and_code_constraint_on_onets_table.rb
+++ db/migrate/20231003233735_add_version_and_code_constraint_on_onets_table.rb
@@ -1,6 +1,11 @@
 class AddVersionAndCodeConstraintOnOnetsTable < ActiveRecord::Migration[7.0]
-  def change
+  def up
     remove_index :onets, :code
     add_index :onets, [:version, :code], unique: true
+  end
+
+  def down
+    remove_index :onets, [:version, :code]
+    add_index :onets, :code, name: :unique_code, unique: true
   end
 end
