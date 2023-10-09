73a046368ade3895cd83a0d5b5a19e38b62e4c6f
Fernando Perales <fer@thoughtbot.com>
2023-10-09 14:46:28 -0600

    Revert debug value

--- app/queries/occupation_standard_elasticsearch_query.rb
+++ app/queries/occupation_standard_elasticsearch_query.rb
@@ -5,7 +5,7 @@ class OccupationStandardElasticsearchQuery
 
   attr_reader :search_params, :offset, :debug
 
-  def initialize(search_params:, offset: 0, debug: true)
+  def initialize(search_params:, offset: 0, debug: false)
     @search_params = search_params
     @offset = offset
     @debug = debug
