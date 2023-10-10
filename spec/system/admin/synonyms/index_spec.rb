6e6d3b33dbd070eb6bf460d41025d8f5fc9ff260
Fernando Perales <fer@thoughtbot.com>
2023-10-10 09:43:25 -0600

    Add system specs for admin synonyms

---
+++ spec/system/admin/synonyms/index_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+RSpec.describe "admin/synonyms/index", :admin do
+  it "can search by word" do
+    create(:synonym, word: "UX", synonyms: "User Experience")
+    create(:synonym, word: "Software Engineer", synonyms: "Dev, Developer")
+
+
+    admin = create(:admin)
+
+    login_as admin
+    visit admin_synonyms_path
+
+    expect(page).to have_gridcell("UX")
+    expect(page).to have_gridcell("Software Engineer")
+
+    visit admin_synonyms_path(search: "Software")
+
+    expect(page).to have_gridcell("Software Engineer")
+    expect(page).to_not have_gridcell("UX")
+  end
+
+  it "can search by synonym" do
+    create(:synonym, word: "UX", synonyms: "User Experience")
+    create(:synonym, word: "Software Engineer", synonyms: "Dev, Developer")
+
+
+    admin = create(:admin)
+
+    login_as admin
+    visit admin_synonyms_path
+
+    expect(page).to have_gridcell("UX")
+    expect(page).to have_gridcell("Software Engineer")
+
+    visit admin_synonyms_path(search: "Dev")
+
+    expect(page).to have_gridcell("Software Engineer")
+    expect(page).to_not have_gridcell("UX")
+  end
+
+end
