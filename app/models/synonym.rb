a1392bdc3aeabe4971b711f587c3779e127736a6
Fernando Perales <fer@thoughtbot.com>
2023-10-11 09:52:33 -0600

    Fix linter issue

--- app/models/synonym.rb
+++ app/models/synonym.rb
@@ -1,7 +1,7 @@
 require "elasticsearch_wrapper/synonyms"
 
 class Synonym < ApplicationRecord
-  COMMA_SEPARATED_LIST_REGEX = /^([a-zA-Z \/\-\_]*,?)+$/
+  COMMA_SEPARATED_LIST_REGEX = /^([a-zA-Z \/\-_]*,?)+$/
   validate :synonyms_list_valid?
   validates :word, :synonyms, presence: true
 
