b8ef21e8ed92dc1875ab7f5cc220e51020dc2f7e
Jennifer Lu <nejul33@gmail.com>
2023-10-11 16:35:06 -0400

    Format onet code when importing

--- app/services/scrape_onet_codes.rb
+++ app/services/scrape_onet_codes.rb
@@ -2,12 +2,17 @@ class ScrapeOnetCodes
   def call
     file = URI.open("https://www.onetonline.org/find/all/All_Occupations.csv?fmt=csv")
     CSV.parse(file, headers: true) do |row|
+      formatted_code = format_code(row["Code"])
       onet = Onet.find_or_initialize_by(
         version: Onet::CURRENT_VERSION,
-        code: row["Code"]
+        code: formatted_code
       )
       onet.update!(title: row["Occupation"])
       OnetWebService.new(onet).call
     end
+  end
+
+  def format_code(code)
+    code.sub(/^(\d{2})\./, '\1-')
   end
 end
--- spec/services/scrape_onet_codes_spec.rb
+++ spec/services/scrape_onet_codes_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe ScrapeOnetCodes do
       onet = create(:onet, title: "Not actually an actuary", code: "15-2011.00")
 
       service = instance_double("OnetWebService", call: nil)
-      expect(OnetWebService).to receive(:new).and_return(service).thrice
-      expect(service).to receive(:call).thrice
+      expect(OnetWebService).to receive(:new).and_return(service).exactly(4).times
+      expect(service).to receive(:call).exactly(4).times
 
       expect {
         described_class.new.call
-      }.to change(Onet, :count).by(2)
+      }.to change(Onet, :count).by(3)
 
       onet.reload
       expect(onet.title).to eq "Actuaries"
@@ -22,12 +22,17 @@ RSpec.describe ScrapeOnetCodes do
       onet1 = Onet.second
       expect(onet1.title).to eq "Accountants and Auditors"
       expect(onet1.code).to eq "13-2011.00"
-      expect(onet.version).to eq "2019"
+      expect(onet1.version).to eq "2019"
 
       onet2 = Onet.third
       expect(onet2.title).to eq "Actors"
       expect(onet2.code).to eq "27-2011.00"
-      expect(onet.version).to eq "2019"
+      expect(onet2.version).to eq "2019"
+
+      onet3 = Onet.fourth
+      expect(onet3.title).to eq "Aerospace Engineers"
+      expect(onet3.code).to eq "17-2011.00"
+      expect(onet3.version).to eq "2019"
     end
   end
 
