763fa43c3a2e9ff6b1eba552a6fead5e4fcbbe6a
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-10-16 09:47:45 -0700

    Add username and password credentials for Elasticsearch (#462)
    connection in production environment

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,19 @@
 require "elasticsearch"
+
 host = ENV.fetch("FOUNDELASTICSEARCH_URL", "http://localhost:9200")
-Elasticsearch::Model.client = Elasticsearch::Client.new(host: host, log: false)
+
+args = {
+  host: host,
+  log: false
+}
+
+if Rails.env.production?
+  args.merge(
+    port: "443",
+    scheme: "https",
+    user: ENV.fetch("ELASTICSEARCH_USER"),
+    password: ENV.fetch("ELASTICSEARCH_PASSWORD")
+  )
+end
+
+Elasticsearch::Model.client = Elasticsearch::Client.new(args)
