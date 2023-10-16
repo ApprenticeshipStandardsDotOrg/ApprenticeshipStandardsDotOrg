dbf3d71c6923716b00f35d4c4d65f704e30abcba
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-10-16 14:34:15 -0700

    Use Elastic Cloud ID to configure Heroku add-on (#466)

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,19 +1,14 @@
 require "elasticsearch"
 
-host = ENV.fetch("FOUNDELASTICSEARCH_URL", "http://localhost:9200")
-
-args = {
-  host: host,
-  log: false
-}
-
-if Rails.env.production?
-  args.merge(
-    port: "443",
-    scheme: "https",
-    user: ENV.fetch("ELASTICSEARCH_USER"),
-    password: ENV.fetch("ELASTICSEARCH_PASSWORD")
-  )
+args = if Rails.env.production?
+  {
+    cloud_id: ENV.fetch("ELASTIC_CLOUD_ID"),
+    user: ENV.fetch("ELASTIC_USER"),
+    password: ENV.fetch("ELASTIC_PASSWORD"),
+    log: false
+  }
+else
+  {log: false}
 end
 
 Elasticsearch::Model.client = Elasticsearch::Client.new(args)
