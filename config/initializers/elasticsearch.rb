aa44732bfcae113591bcfa91b31c71417bd07263
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-10-17 08:52:41 -0700

    Allow Elasticsearch logging to be enabled in production

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,11 +1,12 @@
 require "elasticsearch"
 
 args = if Rails.env.production? && ENV["ELASTIC_CLOUD_ID"].present?
+  enable_logging = ENV.fetch("ELASTIC_LOGGING_ENABLED", "false")
   {
     cloud_id: ENV.fetch("ELASTIC_CLOUD_ID"),
     user: ENV.fetch("ELASTIC_USER"),
     password: ENV.fetch("ELASTIC_PASSWORD"),
-    log: false
+    log: ActiveModel::Type::Boolean.new.cast(enable_logging)
   }
 else
   {log: false}
