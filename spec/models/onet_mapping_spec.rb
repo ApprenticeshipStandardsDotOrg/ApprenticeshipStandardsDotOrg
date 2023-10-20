35063048f5087c8fa7c799c320ffacaad7503fd7
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-10-19 17:04:45 -0700

    Add uniqueness constraint on onet_mapping at the application level

--- spec/models/onet_mapping_spec.rb
+++ spec/models/onet_mapping_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe OnetMapping, type: :model do
 
     expect(onet_mapping).to be_valid
   end
+
+  it "is unique wrt onet_id, next_version_onet_id" do
+    onet_mapping = create(:onet_mapping)
+    new_onet_mapping = build(:onet_mapping, onet: onet_mapping.onet, next_version_onet: onet_mapping.next_version_onet)
+
+    expect(new_onet_mapping).to_not be_valid
+  end
 end
