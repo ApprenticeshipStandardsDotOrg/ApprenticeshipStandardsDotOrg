50ec562550b469b09404dc4eaaf9bf1b8bc420f0
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-11-15 14:58:28 -0800

    Working source file factory

--- spec/factories/source_files.rb
+++ spec/factories/source_files.rb
@@ -2,13 +2,11 @@ FactoryBot.define do
   factory :source_file do
     traits_for_enum :status, SourceFile.statuses
 
-    transient do
-      standards_import { create(:standards_import, :with_files) }
+    initialize_with do
+      standards_import = create(:standards_import, :with_files)
+      SourceFile.where(
+        active_storage_attachment_id: standards_import.files.first.id
+      ).first_or_initialize
     end
-
-    active_storage_attachment_id { standards_import.files.first.id }
-
-#    to_create { |_obj, _context| SourceFile.last }
-    to_create { |obj, _context| obj = StandardsImport.last.files.first.source_file ; obj.save!; obj }
   end
 end
