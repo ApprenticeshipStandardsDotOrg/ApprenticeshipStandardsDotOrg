d801b19ece29363d2c1761a151c9f1d381c4adb4
Jennifer Lu <nejul33@gmail.com>
2023-12-04 10:48:22 -0500

    Lint

--- app/services/chatgpt_service.rb
+++ app/services/chatgpt_service.rb
@@ -12,10 +12,11 @@ class ChatgptService
   def generate_text(prompt)
     response = @client.chat(
       parameters: {
-          model: "gpt-3.5-turbo", 
-          messages: [{ role: "user", content: prompt}], 
-          temperature: 0.7,
-      })
+        model: "gpt-3.5-turbo",
+        messages: [{role: "user", content: prompt}],
+        temperature: 0.7
+      }
+    )
 
     response.dig("choices", 0, "message", "content")
   end
