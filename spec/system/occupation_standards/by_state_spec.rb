658ccacb2cac67a0779f3746c681c2b6f0533e74
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-12-18 14:17:37 -0800

    Spec fix

--- spec/system/occupation_standards/by_state_spec.rb
+++ spec/system/occupation_standards/by_state_spec.rb
@@ -8,21 +8,21 @@ RSpec.describe ":state/occupation_standards" do
     new_york_registration_agency = create(:registration_agency, state: new_york)
     nevada_registration_agency = create(:registration_agency, state: nevada)
 
-    mechanic_from_new_york = create(:occupation_standard,
+    standard_from_new_york = create(:occupation_standard,
       :with_work_processes,
       :with_data_import,
       title: "Mechanic",
       registration_agency: new_york_registration_agency)
-    mechanic_from_nevada = create(:occupation_standard,
+    _standard_from_nevada = create(:occupation_standard,
       :with_work_processes,
       :with_data_import,
-      title: "Mechanic",
+      title: "Pipe Fitter",
       registration_agency: nevada_registration_agency)
 
     visit occupation_standards_by_state_path(state: new_york.abbreviation)
 
-    expect(page).to have_link "Mechanic", href: occupation_standard_path(mechanic_from_new_york)
-    expect(page).not_to have_link "Mechanic", href: occupation_standard_path(mechanic_from_nevada)
+    expect(page).to have_link "Mechanic", href: occupation_standard_path(standard_from_new_york)
+    expect(page).not_to have_link "Pipe Fitter"
   end
 
   it "shows no results when state does not exist" do
