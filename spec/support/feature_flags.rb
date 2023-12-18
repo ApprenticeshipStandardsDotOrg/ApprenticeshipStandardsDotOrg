ff67502cb00442c494a8538c538e59e51da8de2f
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-12-18 15:59:16 -0800

    Add feature flag to avoid warnings

--- spec/support/feature_flags.rb
+++ spec/support/feature_flags.rb
@@ -1,5 +1,9 @@
 require "./spec/support/helpers/stub_feature_flags"
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    Flipper.add(:use_elasticsearch_for_search)
+  end
+
   config.include Helpers::StubFeatureFlags
 end
