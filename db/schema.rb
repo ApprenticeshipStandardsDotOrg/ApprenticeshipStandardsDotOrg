cd3ccce37eaa9001993370a0b129500ff1e98a84
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-12-18 11:42:55 -0800

    Flipper updates
    After upgrading Flipper gem, getting following message:
    
    Your database needs migrated to use the latest Flipper features.
    Run `rails generate flipper:update` and `rails db:migrate`.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_30_195553) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_18_194224) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -115,7 +115,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_195553) do
   create_table "flipper_gates", force: :cascade do |t|
     t.string "feature_key", null: false
     t.string "key", null: false
-    t.string "value"
+    t.text "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true
