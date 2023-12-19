8ddc115a15f7d60a77978929f7a97b9417a82d52
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-12-18 16:10:15 -0800

    Replace more Flipper.enable in specs

--- spec/support/feature_flags.rb
+++ spec/support/feature_flags.rb
@@ -2,9 +2,10 @@ require "./spec/support/helpers/stub_feature_flags"
 
 RSpec.configure do |config|
   config.before(:suite) do
-    Flipper.add(:use_elasticsearch_for_search)
-    Flipper.add(:show_recently_added_section)
     Flipper.add(:recaptcha)
+    Flipper.add(:show_recently_added_section)
+    Flipper.add(:similar_programs_elasticsearch)
+    Flipper.add(:use_elasticsearch_for_search)
   end
 
   config.include Helpers::StubFeatureFlags
