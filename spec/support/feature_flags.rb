f8bf10ca570820c4372f906e36069a95228cf9e3
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-12-19 13:06:52 -0800

    Turn off strict checking for flipper flags in test env

--- spec/support/feature_flags.rb
+++ spec/support/feature_flags.rb
@@ -1,12 +1,5 @@
 require "./spec/support/helpers/stub_feature_flags"
 
 RSpec.configure do |config|
-  config.before(:suite) do
-    Flipper.add(:recaptcha)
-    Flipper.add(:show_recently_added_section)
-    Flipper.add(:similar_programs_elasticsearch)
-    Flipper.add(:use_elasticsearch_for_search)
-  end
-
   config.include Helpers::StubFeatureFlags
 end
