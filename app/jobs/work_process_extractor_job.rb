d669900aaec3b1fcaf3b569fbe387df0e68af44c
Fernando Perales <fer@thoughtbot.com>
2024-01-09 09:06:00 -0600

    Fix linter issues

--- app/jobs/work_process_extractor_job.rb
+++ app/jobs/work_process_extractor_job.rb
@@ -1,10 +1,10 @@
 class WorkProcessExtractorJob < ApplicationJob
   queue_as :default
 
-  def perform()
-    file = '/Users/nina/Documents/ApprenticeshipStandardsDotOrg/tmp/test4.pdf'
+  def perform
+    file = "/Users/nina/Documents/ApprenticeshipStandardsDotOrg/tmp/test4.pdf"
     reader = PDF::Reader.new(file)
-    text = reader.pages.map{|page| page.text}
+    text = reader.pages.map { |page| page.text }
 
     prompt = "Could you identify the work processes and the hours spent in them in this document in a json with name of the process as key and hours as the value? Use 'N/A' when there is no hour. #{text}"
     response = ChatGptGenerateText.new(prompt).call
