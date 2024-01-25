2eb872141b2a1f536527d221202cdb2f739bd1aa
Jen Lu <58493319+jenlu33@users.noreply.github.com>
2024-01-25 10:31:17 -0500

    Add PDFReader gem, set up pdf_reader job to use chatgpt (#558)
    * Add PDFReader gem, set up pdf_reader job to use chatgpt
    
    * Fix linter issues
    
    * Update chatgpt job to correctly return an array of occupations and corresponding type
    
    * Create specs for chatgpt job
    
    * Lint
    
    * Fix URI security risk
    
    * Adjust chatgpt-service spec
    
    * Add mocks for pdfreader
    
    * Add stub request for URI
    
    * Lint
    
    * Adjust spec to not need localhost:3000
    
    * Remove puts
    
    ---------
    
    Co-authored-by: Fernando Perales <fer@thoughtbot.com>

---
+++ spec/jobs/pdf_reader_job_spec.rb
@@ -0,0 +1,47 @@
+require "rails_helper"
+
+RSpec.describe PdfReaderJob do
+  describe "#perform" do
+    let(:pdf_file) { "" }
+
+    before do
+      allow_any_instance_of(SourceFile).to receive(:url).and_return("http://example.com/mock.pdf")
+      stub_request(:get, "http://example.com/mock.pdf").to_return(status: 200, body: pdf_file)
+    end
+
+    it "returns if source file is not a pdf" do
+      source_file = create(:source_file)
+      allow_any_instance_of(SourceFile).to receive(:pdf?).and_return false
+
+      expect(described_class.new.perform(source_file.id)).to be nil
+    end
+
+    it "returns an array of templates with ChatGPT responses" do
+      source_file = create(:source_file)
+      allow_any_instance_of(SourceFile).to receive(:pdf?).and_return true
+
+      stub_request(:get, source_file.url).to_return(status: 200, body: "", headers: {})
+      reader_mock = instance_double "PDF::Reader"
+
+      allow(PDF::Reader).to receive(:new).and_return(reader_mock)
+      allow(reader_mock).to receive(:pages).and_return([instance_double("PDF::Reader::Page", text: "Welder (Industrial)\n(Competency based)\n\n")])
+
+      allow(
+        ChatGptGenerateText
+      ).to receive(:new).with(
+        "Create an array of the occupation(s) from the text. Return as a JSON array [\"Welder (Industrial)\\n(Competency based)\\n\\n\"]"
+      ).and_return chat_gpt_generator_mock("[\"Welder (Industrial)\"]")
+
+      allow(
+        ChatGptGenerateText
+      ).to receive(:new).with(
+        "Please fill out the template based on the given information for this occupation: Welder (Industrial) and return as JSON array information:[\"Welder (Industrial)\\n(Competency based)\\n\\n\"] template: { \"Title\": \"\", \"Type\": \"(Time based, Competency based, or Hybrid)\" }"
+      ).and_return chat_gpt_generator_mock("[\n  {\n    \"Title\": \"Welder (Industrial)\",\n    \"Type\": \"Competency based\"\n  }\n]")
+      expect(described_class.new.perform(source_file.id)).to eq [{"Title" => "Welder (Industrial)", "Type" => "Competency based"}]
+    end
+  end
+end
+
+def chat_gpt_generator_mock(value)
+  OpenStruct.new(call: value)
+end
