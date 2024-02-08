60c42b5806c09ee8f44f8d194ea3161b91f87e41
Jeanine Soterwood <jeanine@thoughtbot.com>
2024-02-08 14:07:26 -0800

    Add DocToPdfConverter job that calls the service

---
+++ spec/jobs/doc_to_pdf_converter_job_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe DocToPdfConverterJob, "#perform", type: :job do
+  it "calls DocToPdfConverter service" do
+    source_file = create(:source_file)
+
+    expect(DocToPdfConverter).to receive(:convert).with(source_file)
+
+    described_class.new.perform(source_file)
+  end
+end
