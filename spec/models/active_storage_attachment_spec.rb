5d28093f05deb723cbe1e8200fabb45205592ec0
Jeanine Soterwood <jeanine@thoughtbot.com>
2024-02-08 16:11:21 -0800

    Do not display converted file to user after they upload docx file

--- spec/models/active_storage_attachment_spec.rb
+++ spec/models/active_storage_attachment_spec.rb
@@ -13,4 +13,22 @@ RSpec.describe ActiveStorage::Attachment, type: :model do
     expect { attachment.reload }.to raise_error(ActiveRecord::RecordNotFound)
     expect { source_file.reload }.to raise_error(ActiveRecord::RecordNotFound)
   end
+
+  describe "#has_linked_original_file?" do
+    it "is true if source file has original_source_file present" do
+      original_source_file = create(:source_file)
+      source_file = create(:source_file)
+      source_file.update!(original_source_file: original_source_file)
+      attachment = source_file.active_storage_attachment
+
+      expect(attachment).to have_linked_original_file
+    end
+
+    it "is false if source file does not have original_source_file present" do
+      source_file = create(:source_file)
+      attachment = source_file.active_storage_attachment
+
+      expect(attachment).to_not have_linked_original_file
+    end
+  end
 end
