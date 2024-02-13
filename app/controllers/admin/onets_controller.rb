05ae7b11a05094524745be4c62ccf5535bf877a4
Jeanine Soterwood <jeanine@thoughtbot.com>
2024-02-13 09:13:25 -0800

    Add default sorting options for all admin resources (#637)

--- app/controllers/admin/onets_controller.rb
+++ app/controllers/admin/onets_controller.rb
@@ -1,4 +1,13 @@
 module Admin
   class OnetsController < Admin::ApplicationController
+    private
+
+    def default_sorting_attribute
+      :code
+    end
+
+    def default_sorting_direction
+      :asc
+    end
   end
 end
