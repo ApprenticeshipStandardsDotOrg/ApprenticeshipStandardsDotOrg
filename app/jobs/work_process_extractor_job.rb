406e6937764deee241cde9b1fd030d5b5cb4d30f
Richard Newman <114449566+rdnewmanbot@users.noreply.github.com>
2024-02-26 12:27:13 -0700

    slight refactor to new job for readability

--- app/jobs/work_process_extractor_job.rb
+++ app/jobs/work_process_extractor_job.rb
@@ -2,12 +2,22 @@ class WorkProcessExtractorJob < ApplicationJob
   queue_as :default
 
   def perform
-    file = "/Users/nina/Documents/ApprenticeshipStandardsDotOrg/tmp/test4.pdf"
-    reader = PDF::Reader.new(file)
-    text = reader.pages.map { |page| page.text }
-
-    prompt = "Could you identify the work processes and the hours spent in them in this document in a json with name of the process as key and hours as the value? Use 'N/A' when there is no hour. #{text}"
     response = ChatGptGenerateText.new(prompt).call
     puts response
+  end
+
+private
+
+  def prompt
+    "Could you identify the work processes and the hours spent in them in this document " \
+    "in a json with name of the process as key and hours as the value? Use 'N/A' when there " \
+    "is no hour. #{text}"
+  end
+
+  def text
+    filepath = Rails.root.join("tmp", "test4.pdf")
+    reader = PDF::Reader.new(filepath)
+
+    reader.pages.map(&:text)
   end
 end
