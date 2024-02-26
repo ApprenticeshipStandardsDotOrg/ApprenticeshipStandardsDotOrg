3d81f840555857bd45603ada6f13ae214247135b
Richard Newman <114449566+rdnewmanbot@users.noreply.github.com>
2024-02-26 12:25:11 -0700

    deepen error support in chatgpt service

--- app/services/chat_gpt_generate_text.rb
+++ app/services/chat_gpt_generate_text.rb
@@ -18,5 +18,33 @@ class ChatGptGenerateText
     )
 
     response.dig("choices", 0, "message", "content")
+  rescue Faraday::ResourceNotFound => e
+    on_error(e)
   end
+
+  private
+
+  def on_error(err)
+    error_hash = err.response.dig(:body, "error")
+    case error_hash["type"]
+    when "invalid_request_error"
+      raise InvalidRequestError.new(error_hash:)
+    else
+      raise UnknownError.new(error_hash:)
+    end
+  end
+
+  class ChatGptError < StandardError
+    attr_reader :type, :param, :code
+
+    def initialize(error_hash:)
+      message = error_hash["message"]
+      @type = error_hash["type"]
+      @param = error_hash["param"]
+      @code = error_hash["code"]
+      super("#{message} (code: #{@code}; param: #{param || '-nil-'})")
+    end
+  end
+  class InvalidRequestError < ChatGptError; end
+  class UnknownError < ChatGptError; end
 end
