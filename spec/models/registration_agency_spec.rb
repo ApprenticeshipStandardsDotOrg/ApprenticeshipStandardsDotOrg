768c45d444024d38f648500af2cd22a9a10eb555
Fernando Perales <fer@thoughtbot.com>
2024-03-04 13:08:29 -0600

    Add specs for RegistrationAgency.registration_agency_for_national_program

--- spec/models/registration_agency_spec.rb
+++ spec/models/registration_agency_spec.rb
@@ -16,6 +16,18 @@ RSpec.describe RegistrationAgency, type: :model do
     expect(new_ra).to be_valid
   end
 
+  describe ".registration_agency_for_national_program" do
+    it "returns nil if there is no registration agency without an state" do
+      expect(described_class.registration_agency_for_national_program).to be nil
+    end
+
+    it "returns registration agency without an state if present" do
+      registration_agency = create(:registration_agency, state: nil)
+
+      expect(described_class.registration_agency_for_national_program).to eq registration_agency
+    end
+  end
+
   describe "#to_s" do
     it "returns state name and agency type" do
       ca = build_stubbed(:state, name: "California")
