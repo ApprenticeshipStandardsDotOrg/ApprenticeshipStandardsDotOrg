eb4694770ec894526a80acce881af76ab3dde7e9
Richard Newman <114449566+rdnewmanbot@users.noreply.github.com>
2024-03-06 07:59:34 -0700

    clean up import in prep for competencies

--- app/services/import_via_chatgpt/process_data_import.rb
+++ app/services/import_via_chatgpt/process_data_import.rb
@@ -5,40 +5,47 @@ module ImportViaChatgpt
       @text = PdfFile.text(attachment)
     end
 
-    # TODO: move 'occ' somewhere else? At least name it better!
-    def work_processes(occ)
-      @work_processes ||= import_work_processes(occ)
+    # TODO: collect occupation_standard from document
+    def work_processes(occupation_standard)
+      @work_processes ||= import_work_processes(occupation_standard)
     end
 
     private
 
     attr_reader :attachment, :text
 
-    def import_work_processes(occ)
-      response = ChatGptGenerateText.new(prompt(:work_processes)).call
-
-      JSON.parse(response).map do |description, hour_bounds|
-        WorkProcesses.import(
-          occupation_standard: occ,
-          description:,
-          hour_bounds:
-        )
+    # @returns [Array<WorkProcess>]
+    def import_work_processes(occupation_standard)
+      JSON.parse(inferred_work_processes).map do |description, hour_bounds|
+        WorkProcesses.import(occupation_standard:, description:, hour_bounds:)
       end
     end
 
-    def prompt(key)
-      "#{prepended_prompt(:work_processes)} #{text}"
+    # @returns [String]
+    def inferred_work_processes
+      ChatGptGenerateText.new(prompt(:work_processes)).call
     end
 
-    def prepended_prompt(key)
-      @prepended_prompt ||= {
-        work_processes: "Could you identify the work processes and the hours spent in them in this " \
-          "document in a json with name of the process as key and hours as an array with the " \
-          "values of minimum and maximum hours? Use 'N/A' when there is no hour, and fill both " \
-          "values of the array with the same number when there is only one value for hour. "
+    def prompt(key)
+      "#{prompts(key)} #{text}"
+    end
+
+    def prompts(key)
+      @prompts ||= {
+        work_processes: "Identify the work processes and the hours spent in them in this " \
+          "document as json with name of the process as key and hours as an array with the " \
+          "values of minimum and maximum hours? If no work processes can be found, return an " \
+          "empty json object without further explanation. Use 'N/A' when there is no hour, " \
+          "and fill both values of the array with the same number when there is only one value " \
+          "for hour. ",
+        competencies: "Identify the competencies and the work processes they are " \
+          "related to in this document as json with name of the competencies as key and " \
+          "their related work processes as an array? Only consider something to be a competency " \
+          "if clearly marked as one; if none can be found, return an empty json object without " \
+          "further explanation. Use an empty array where there are no related work processes. "
       }
 
-      @prepended_prompt.fetch(key)
+      @prompts.fetch(key)
     end
   end
 end
