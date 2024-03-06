68ba2cdef2a556070034f5af102f4aa856f61a0a
Richard Newman <114449566+rdnewmanbot@users.noreply.github.com>
2024-03-06 08:23:55 -0700

    linting fixes

--- spec/models/pdf_file_spec.rb
+++ spec/models/pdf_file_spec.rb
@@ -1,18 +1,18 @@
 require "rails_helper"
 
 RSpec.describe PdfFile, type: :model do
-  describe 'object' do
-    it 'can return underlying PDF::File object' do
+  describe "object" do
+    it "can return underlying PDF::File object" do
       expect(described_object.pdf_reader).to be_a(PDF::Reader)
     end
 
-    it 'returns expected text' do
+    it "returns expected text" do
       content = "6th period    31-36 months 4501-5400 OJT"
       expect(described_object.text).to match(/#{content}/)
       expect(described_object.text.length).to be > 7000
     end
 
-    it 'still allows other methods of PDF::Reader' do
+    it "still allows other methods of PDF::Reader" do
       expect(described_object.page_count).to eq 3
     end
 
@@ -21,8 +21,8 @@ RSpec.describe PdfFile, type: :model do
     end
   end
 
-  describe '.text class method' do
-    it 'returns expected text' do
+  describe ".text class method" do
+    it "returns expected text" do
       file = file_fixture("process_data_example_01.pdf")
       content = "6th period    31-36 months 4501-5400 OJT"
 
