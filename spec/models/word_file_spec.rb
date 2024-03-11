a24137af3a63be87b9e237b4ebae16af6b291db3
Jeanine Soterwood <jeanine@thoughtbot.com>
2024-03-11 08:13:51 -0700

    Account for .doc and .pdf files as bulletin file types (#680)
    The existing code assumes that all Bulletins will be of .docx file
    type, but this is not the case. Some Bulletins are of .doc format
    and others are of .pdf format. These changes unify code to
    consider a generic "word" filetype, and ensure that we do not
    attempt to extract embedded objects from pdf files.

--- spec/models/word_file_spec.rb
+++ spec/models/word_file_spec.rb
@@ -1,16 +1,18 @@
 require "rails_helper"
 
-RSpec.describe DocxFile do
+RSpec.describe WordFile do
   include ActionDispatch::TestProcess::FixtureFile
 
   describe ".has_embedded_files?" do
     it "it is true if the file has embeds" do
       file = file_fixture_upload("docx_file_attachments.docx")
+
       expect(described_class.has_embedded_files?(file)).to be_truthy
     end
 
     it "is false if the file doesn't have embeds" do
       file = file_fixture_upload("document.docx")
+
       expect(described_class.has_embedded_files?(file)).to be_falsey
     end
 
