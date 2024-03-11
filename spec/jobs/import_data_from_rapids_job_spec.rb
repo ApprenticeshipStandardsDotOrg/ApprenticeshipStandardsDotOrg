f53a75fef6b809db9ed701475ce7ed572d046ee2
Fernando Perales <fer@thoughtbot.com>
2024-03-11 09:25:59 -0600

    Add specs

--- spec/jobs/import_data_from_rapids_job_spec.rb
+++ spec/jobs/import_data_from_rapids_job_spec.rb
@@ -28,6 +28,60 @@ RSpec.describe ImportDataFromRAPIDSJob, type: :job do
         expect(occupation_standard.ojt_type).to eq "competency"
       end
     end
+
+    context "when receiving a time-based standard" do
+      it "creates the associated records" do
+        stub_get_token!
+
+        mi = create(:state, abbreviation: "MI")
+        create(:registration_agency, state: mi, agency_type: :oa)
+
+        occupation_standard_response = create_list(:rapids_api_occupation_standard, 1, :time)
+        rapids_response = create(:rapids_response, totalCount: 1, wps: occupation_standard_response)
+
+        stub_rapids_api_response(
+          {
+            batchSize: ImportDataFromRAPIDSJob::PER_PAGE_SIZE,
+            startIndex: 1
+          },
+          rapids_response
+        )
+        expect {
+          described_class.perform_now
+        }.to change(OccupationStandard, :count).to eq 1
+
+        occupation_standard = OccupationStandard.last
+
+        expect(occupation_standard.ojt_type).to eq "time"
+      end
+    end
+
+    context "when receiving a hybrid standard" do
+      it "creates the associated records" do
+        stub_get_token!
+
+        mi = create(:state, abbreviation: "MI")
+        create(:registration_agency, state: mi, agency_type: :oa)
+
+        occupation_standard_response = create_list(:rapids_api_occupation_standard, 1, :hybrid)
+        rapids_response = create(:rapids_response, totalCount: 1, wps: occupation_standard_response)
+
+        stub_rapids_api_response(
+          {
+            batchSize: ImportDataFromRAPIDSJob::PER_PAGE_SIZE,
+            startIndex: 1
+          },
+          rapids_response
+        )
+        expect {
+          described_class.perform_now
+        }.to change(OccupationStandard, :count).to eq 1
+
+        occupation_standard = OccupationStandard.last
+
+        expect(occupation_standard.ojt_type).to eq "hybrid"
+      end
+    end
   end
 end
 
