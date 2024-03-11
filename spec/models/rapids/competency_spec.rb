024a29c2ab899a426d0d28bd89a9065341301645
Fernando Perales <fer@thoughtbot.com>
2024-03-11 07:42:30 -0600

    Add RAPIDS Competency model

---
+++ spec/models/rapids/competency_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe RAPIDS::Competency, type: :model do
+  describe ".initialize_from_response" do
+    it "returns organization with correct data" do
+      competency_response = create(:rapids_api_competency, title: "Competency #1")
+
+      competency = RAPIDS::Competency.initialize_from_response(competency_response)
+
+      expect(competency.title).to eq "Competency #1"
+    end
+  end
+end
