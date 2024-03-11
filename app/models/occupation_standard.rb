c5284c32323e1f26a0557ed88dfb50963764443c
Fernando Perales <fer@thoughtbot.com>
2024-03-11 13:38:27 -0600

    Refactor conditional

--- app/models/occupation_standard.rb
+++ app/models/occupation_standard.rb
@@ -254,7 +254,7 @@ class OccupationStandard < ApplicationRecord
   end
 
   def public_document?
-    !!(source_file&.public_document || standards_import&.public_document)
+    (source_file&.public_document || standards_import&.public_document).present?
   end
 
   def original_file_url
