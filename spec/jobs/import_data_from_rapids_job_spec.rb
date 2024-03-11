e9bbe6eaf686b621adb8530982a3c557722032b9
Fernando Perales <fer@thoughtbot.com>
2024-03-11 09:47:01 -0600

    Add pagination specs

--- spec/jobs/import_data_from_rapids_job_spec.rb
+++ spec/jobs/import_data_from_rapids_job_spec.rb
@@ -83,6 +83,53 @@ RSpec.describe ImportDataFromRAPIDSJob, type: :job do
       end
     end
   end
+
+  context "pagination" do
+    it "performs more than one call with correct arguments when records exceed batch size" do
+      stub_get_token!
+      stub_const("ImportDataFromRAPIDSJob::PER_PAGE_SIZE", 1)
+
+      mi = create(:state, abbreviation: "MI")
+      create(:registration_agency, state: mi, agency_type: :oa)
+
+      (first_occupation, second_occupation, third_occupation) = create_list(:rapids_api_occupation_standard, 3, :hybrid)
+
+      first_rapids_response = create(:rapids_response, totalCount: 3, wps: [first_occupation])
+      second_rapids_response = create(:rapids_response, totalCount: 3, wps: [second_occupation])
+      third_rapids_response = create(:rapids_response, totalCount: 3, wps: [third_occupation])
+
+      expect_any_instance_of(RAPIDS::API).to receive(:get).with("/wps", {
+        batchSize: 1,
+        startIndex: 1
+      }).and_return(
+        OpenStruct.new(
+          parsed: first_rapids_response
+        )
+      )
+
+      expect_any_instance_of(RAPIDS::API).to receive(:get).with("/wps", {
+        batchSize: 1,
+        startIndex: 2
+      }).and_return(
+        OpenStruct.new(
+          parsed: second_rapids_response
+        )
+      )
+
+      expect_any_instance_of(RAPIDS::API).to receive(:get).with("/wps", {
+        batchSize: 1,
+        startIndex: 3
+      }).and_return(
+        OpenStruct.new(
+          parsed: third_rapids_response
+        )
+      )
+
+      expect {
+        described_class.perform_now
+      }.to change(OccupationStandard, :count).to eq 3
+    end
+  end
 end
 
 def stub_get_token!
