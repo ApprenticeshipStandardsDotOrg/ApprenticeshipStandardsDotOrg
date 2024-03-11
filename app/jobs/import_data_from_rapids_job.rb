4847e9db17f04dd6bafd547df605fe5319ea5368
Fernando Perales <fer@thoughtbot.com>
2024-03-11 08:45:58 -0600

    Add ImportDataFromRAPIDSJob

---
+++ app/jobs/import_data_from_rapids_job.rb
@@ -0,0 +1,36 @@
+class ImportDataFromRAPIDSJob < ApplicationJob
+  queue_as :default
+
+  PER_PAGE_SIZE = 50
+
+  def perform
+    service = RAPIDS::API.call
+    start_index = 1
+
+    loop do
+      response = service.work_processes(
+        batchSize: PER_PAGE_SIZE,
+        startIndex: start_index
+      )
+      parsed_response = response.parsed
+      occupation_standards = process_api_response(parsed_response)
+      occupation_standards.map(&:save!)
+      total_records = parsed_response["totalCount"]
+      start_index += PER_PAGE_SIZE
+
+      break if start_index > total_records
+    end
+  end
+
+  def process_api_response(response)
+    response["wps"].map do |occupation_standard_response|
+      process_occupation_standard(occupation_standard_response)
+    end
+  end
+
+  def process_occupation_standard(occupation_standard_response)
+    RAPIDS::OccupationStandard.initialize_from_response(
+      occupation_standard_response
+    )
+  end
+end
---
+++ spec/jobs/import_data_from_rapids_job_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+
+RSpec.describe ImportDataFromRAPIDSJob, type: :job do
+  describe "#perform" do
+    context "when receiving a competency-based standard" do
+      it "creates the associated records" do
+        stub_get_token!
+
+        mi = create(:state, abbreviation: "MI")
+        create(:registration_agency, state: mi, agency_type: :oa)
+
+        occupation_standard_response = create_list(:rapids_api_occupation_standard, 1, :competency)
+        rapids_response = create(:rapids_response, totalCount: 1, wps: occupation_standard_response)
+
+        stub_rapids_api_response(
+          {
+            batchSize: ImportDataFromRAPIDSJob::PER_PAGE_SIZE,
+            startIndex: 1
+          },
+          rapids_response
+        )
+        expect {
+          described_class.perform_now
+        }.to change(OccupationStandard, :count).to eq 1
+
+        occupation_standard = OccupationStandard.last
+
+        expect(occupation_standard.ojt_type).to eq "competency"
+
+      end
+    end
+  end
+end
+
+def stub_get_token!
+  allow_any_instance_of(RAPIDS::API).to receive(:get_token!).and_return "xxx"
+end
+
+def stub_rapids_api_response(arguments, response)
+  allow_any_instance_of(RAPIDS::API).to receive(:get).with("/wps", arguments).and_return(
+    OpenStruct.new(
+      parsed: response,
+    )
+  )
+end
