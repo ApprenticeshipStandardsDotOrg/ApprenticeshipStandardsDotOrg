1a122ca65acb6882a8caa2faf935f3b2eb4d37db
Fernando Perales <fer@thoughtbot.com>
2024-03-14 11:35:10 -0600

    Process Competencies

--- spec/models/rapids/work_process_spec.rb
+++ spec/models/rapids/work_process_spec.rb
@@ -16,5 +16,21 @@ RSpec.describe RAPIDS::WorkProcess, type: :model do
       expect(work_process.minimum_hours).to eq 20
       expect(work_process.maximum_hours).to eq 200
     end
+
+    it "returns work process with competencies if available" do
+      work_process_response = create(
+        :rapids_api_detailed_work_activity,
+        tasks: ["Competency #1; Competency #2; Competency #3"]
+      )
+
+      work_process = described_class.initialize_from_response(work_process_response)
+
+      (first_competency, second_competency, third_competency) = work_process.competencies
+
+      expect(work_process.competencies.size).to eq 3
+      expect(first_competency.title).to eq "Competency #1"
+      expect(second_competency.title).to eq "Competency #2"
+      expect(third_competency.title).to eq "Competency #3"
+    end
   end
 end
