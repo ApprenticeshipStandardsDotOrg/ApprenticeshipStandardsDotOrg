2d14189af229c7ee509c755e5b61627156ece162
Fernando Perales <fer@thoughtbot.com>
2024-03-15 09:57:22 -0600

    RAPIDS API - Import Competencies (#691)
    * Process Competencies
    
    * Handle extra or missing spaces between semicolon
    
    * Update spec/models/rapids/work_process_spec.rb
    
    Co-authored-by: Jeanine Soterwood <jeanine@thoughtbot.com>
    
    ---------
    
    Co-authored-by: Jeanine Soterwood <jeanine@thoughtbot.com>

--- app/models/rapids/work_process.rb
+++ app/models/rapids/work_process.rb
@@ -1,11 +1,37 @@
 module RAPIDS
   class WorkProcess
-    def self.initialize_from_response(work_process_data)
-      ::WorkProcess.new(
-        title: work_process_data["title"],
-        minimum_hours: work_process_data["minHours"],
-        maximum_hours: work_process_data["maxHours"]
-      )
+    class << self
+      def initialize_from_response(work_process_data)
+        tasks = work_process_data["tasks"]
+
+        work_process = ::WorkProcess.new(
+          title: work_process_data["title"],
+          minimum_hours: work_process_data["minHours"],
+          maximum_hours: work_process_data["maxHours"]
+        )
+
+        if competencies_available?(tasks)
+          competencies = process_competencies(tasks)
+          work_process.competencies = competencies
+        end
+
+        work_process
+      end
+
+      private
+
+      def competencies_available?(tasks)
+        tasks&.first&.present?
+      end
+
+      def process_competencies(tasks)
+        tasks_titles = tasks.first.split(/\s?;\s?/)
+        tasks_titles.map do |task_title|
+          Competency.initialize_from_response({
+            "title" => task_title
+          })
+        end
+      end
     end
   end
 end
--- spec/factories/rapids_api/detailed_work_activities.rb
+++ spec/factories/rapids_api/detailed_work_activities.rb
@@ -3,9 +3,23 @@ FactoryBot.define do
     skip_create
 
     sequence(:title) { |n| "Detailed Work Activity #{n}" }
-    sequence(:task) { |n| ["Task #{n}"] }
+    sequence(:tasks) { |n| [""] }
 
     initialize_with { attributes.stringify_keys }
+
+    transient do
+      with_tasks { 0 }
+    end
+
+    after(:create) do |detailed_work_activity, context|
+      if context.with_tasks.positive?
+        detailed_work_activity["tasks"] = [
+          (1..context.with_tasks).map do |n|
+            "Competency #{n}"
+          end.to_sentence(words_connector: "; ", two_words_connector: "; ", last_word_connector: "; ")
+        ]
+      end
+    end
 
     factory :rapids_api_detailed_work_activity_for_hybrid do
       minHours { 200 }
