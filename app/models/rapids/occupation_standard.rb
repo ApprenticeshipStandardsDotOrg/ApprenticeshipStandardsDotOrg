a269a8125b32e0f1757a82219a5f9119a810b42a
Fernando Perales <fer@thoughtbot.com>
2024-03-19 12:22:11 -0600

    Associate Occupation with Occupation Standard

--- app/models/rapids/occupation_standard.rb
+++ app/models/rapids/occupation_standard.rb
@@ -8,14 +8,17 @@ module RAPIDS
 
     class << self
       def initialize_from_response(response)
+        rapids_code = sanitize_rapids_code(response["rapidsCode"])
+        onet_code = response["onetSocCode"]
         ::OccupationStandard.new(
           title: fix_encoding(response["occupationTitle"]),
           onet_code: response["onetSocCode"],
-          rapids_code: response["rapidsCode"],
+          rapids_code: rapids_code,
           ojt_type: ojt_type(response["occType"]),
           registration_agency: find_registration_agency_by_sponsor_number(
             response["sponsorNumber"]
-          )
+          ),
+          occupation: find_occupation(rapids_code, onet_code)
         )
       end
 
@@ -47,6 +50,28 @@ module RAPIDS
 
       def fix_encoding(text)
         text.encode("UTF-8", "UTF-8", invalid: :replace, replace: "")
+      end
+
+      def find_occupation(rapids_code, onet_code)
+        find_occupation_by_rapids_code(rapids_code) || find_occupation_by_onet_code(onet_code)
+      end
+
+      def find_occupation_by_rapids_code(rapids_code)
+        Occupation.find_by(rapids_code: rapids_code)
+      end
+
+      def find_occupation_by_onet_code(onet_code)
+        Occupation.includes(:onet).find_by(
+          onet: {
+            code: onet_code
+          }
+        )
+      end
+
+      def sanitize_rapids_code(rapids_code)
+        return if rapids_code.blank?
+
+        rapids_code.gsub(/[A-Za-z]+\z/, "").ljust(4, "0")
       end
     end
   end
