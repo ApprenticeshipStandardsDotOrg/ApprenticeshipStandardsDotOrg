b924a74dd427937a5d71377fe8835756f8842abe
Fernando Perales <fer@thoughtbot.com>
2024-03-19 13:50:32 -0600

    Upgrade pagy to 7.0.11
    * Override stylesheet to only apply styles to link that have an href
    * Replace < and > with < Prev and Next >
    * Use old default for page size

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,3 +1,8 @@
 require "pagy/extras/overflow"
 require "pagy/extras/elasticsearch_rails"
 Pagy::DEFAULT[:overflow] = :last_page
+Pagy::DEFAULT[:size] = [1, 4, 4, 1]
+
+Pagy::I18n.load(
+  { locale: 'en', filepath: 'config/locales/pagy.yml' }
+)
