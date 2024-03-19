8f86b3c29d0ec00bc4268c2cbae164123566d30d
Fernando Perales <fer@thoughtbot.com>
2024-03-19 11:18:15 -0600

    Return valid work processes only (#698)
    * Return valid work processes only
    
    * Add occupation standard before validation

--- spec/jobs/import_data_from_rapids_job_spec.rb
+++ spec/jobs/import_data_from_rapids_job_spec.rb
@@ -150,6 +150,42 @@ RSpec.describe ImportDataFromRAPIDSJob, type: :job do
         }.to change(OccupationStandard, :count).to eq 3
       end
     end
+
+    context "invalid records" do
+      it "saves the occupation standard discarding invalid work processes" do
+        stub_get_token!
+
+        mi = create(:state, abbreviation: "MI")
+        create(:registration_agency, state: mi, agency_type: :oa)
+
+        invalid_detailed_work_activity = create_list(
+          :rapids_api_detailed_work_activity_for_hybrid,
+          1,
+          title: ""
+        )
+
+        occupation_standard_response = create_list(
+          :rapids_api_occupation_standard,
+          1,
+          :competency,
+          dwas: invalid_detailed_work_activity
+        )
+
+        rapids_response = create(:rapids_response, totalCount: 1, wps: occupation_standard_response)
+
+        stub_rapids_api_response(
+          {
+            batchSize: ImportDataFromRAPIDSJob::PER_PAGE_SIZE,
+            startIndex: 1
+          },
+          rapids_response
+        )
+        expect {
+          described_class.perform_now
+        }.to change(OccupationStandard, :count).by(1).and \
+          change(WorkProcess, :count).by(0)
+      end
+    end
   end
 end
 
