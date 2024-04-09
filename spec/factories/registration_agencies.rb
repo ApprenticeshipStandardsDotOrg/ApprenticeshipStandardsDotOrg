e568f9895a410adc339f23b4a0666c801e67e9e8
Fernando Perales <fer@thoughtbot.com>
2024-04-09 10:11:18 -0600

    Update factory to use trait to build the state

--- spec/factories/registration_agencies.rb
+++ spec/factories/registration_agencies.rb
@@ -2,11 +2,22 @@ FactoryBot.define do
   factory :registration_agency do
     traits_for_enum :agency_type, RegistrationAgency.agency_types
 
-    state
     agency_type { :oa }
 
     trait :for_national_program do
       state { nil }
+    end
+
+    transient do
+      for_state_abbreviation { nil }
+    end
+
+    after(:build) do |registration_agency, context|
+      registration_agency.state ||= if (context.for_state_abbreviation)
+        FactoryBot.build(:state, abbreviation: context.for_state_abbreviation)
+      else
+        FactoryBot.build(:state)
+      end
     end
   end
 end
