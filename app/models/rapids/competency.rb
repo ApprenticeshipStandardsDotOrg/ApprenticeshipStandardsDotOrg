faf40a943191b6c010642681be034bb1aa868b1a
Fernando Perales <fer@thoughtbot.com>
2024-04-15 10:54:00 -0600

    Add sort order to competencies from Work Process (#729)

--- app/models/rapids/competency.rb
+++ app/models/rapids/competency.rb
@@ -2,7 +2,8 @@ module RAPIDS
   class Competency
     def self.initialize_from_response(competency_data)
       ::Competency.new(
-        title: competency_data["title"]
+        title: competency_data["title"],
+        sort_order: competency_data["sort_order"]
       )
     end
   end
