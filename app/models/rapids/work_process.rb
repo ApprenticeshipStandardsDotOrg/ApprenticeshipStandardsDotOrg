7d62fa5bfc34a83e32a3956ba9af02fc5fc90649
Fernando Perales <fer@thoughtbot.com>
2024-04-16 11:37:32 -0600

    Create competencies only when occupation is not time-based

--- app/models/rapids/work_process.rb
+++ app/models/rapids/work_process.rb
@@ -1,8 +1,9 @@
 module RAPIDS
   class WorkProcess
     class << self
-      def initialize_from_response(work_process_data)
+      def initialize_from_response(work_process_data, ojt_type: nil)
         tasks = work_process_data["tasks"]
+        tasks_titles = tasks.first.split(/\s?;\s?/)
 
         work_process = ::WorkProcess.new(
           title: work_process_data["title"],
@@ -10,9 +11,13 @@ module RAPIDS
           maximum_hours: work_process_data["maxHours"]
         )
 
-        if competencies_available?(tasks)
-          competencies = process_competencies(tasks)
-          work_process.competencies = competencies
+        if competencies_available?(tasks_titles)
+          if ojt_type == :time
+            work_process.description = tasks_titles.join("; ")
+          else
+            competencies = process_competencies(tasks_titles)
+            work_process.competencies = competencies
+          end
         end
 
         work_process
@@ -20,12 +25,11 @@ module RAPIDS
 
       private
 
-      def competencies_available?(tasks)
-        tasks&.first&.present?
+      def competencies_available?(tasks_titles)
+        tasks_titles.any?
       end
 
-      def process_competencies(tasks)
-        tasks_titles = tasks.first.split(/\s?;\s?/)
+      def process_competencies(tasks_titles)
         tasks_titles.map.with_index do |task_title, index|
           Competency.initialize_from_response({
             "title" => task_title,
