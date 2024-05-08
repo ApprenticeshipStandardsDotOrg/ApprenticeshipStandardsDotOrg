b922ef8b0c0fb253a8b18ea140c5247c483ce646
Jeanine Soterwood <jeanine@thoughtbot.com>
2024-05-08 13:22:46 -0700

    Refactor

--- app/controllers/admin/data_imports_controller.rb
+++ app/controllers/admin/data_imports_controller.rb
@@ -3,74 +3,43 @@ module Admin
     before_action :set_parent, except: [:show]
 
     def new
+      resource = new_resource
       if Flipper.enabled?(:show_imports_in_administrate)
-        resource = new_resource
-        resource.import = @import
-        authorize_resource(resource)
-        render locals: {
-          page: Administrate::Page::Form.new(dashboard, resource)
-        }
+        resource.import = @parent
       else
-        resource = new_resource
-        resource.source_file = @source_file
-        authorize_resource(resource)
-        render locals: {
-          page: Administrate::Page::Form.new(dashboard, resource)
-        }
+        resource.source_file = @parent
       end
+      authorize_resource(resource)
+      render locals: {
+        page: Administrate::Page::Form.new(dashboard, resource)
+      }
     end
 
     def create
-      if Flipper.enabled?(:show_imports_in_administrate)
-        data_import = @import.data_imports.build(resource_params)
-        data_import.user = current_user
-        authorize_resource(data_import)
+      data_import = @parent.data_imports.build(resource_params)
+      data_import.user = current_user
+      authorize_resource(data_import)
 
-        if data_import.save!
-          ProcessDataImportJob.perform_later(data_import: data_import, last_file: last_file_flag)
+      if data_import.save!
+        ProcessDataImportJob.perform_later(data_import: data_import, last_file: last_file_flag)
 
-          redirect_to(
-            after_data_import_created_path(@import, data_import),
-          )
-        else
-          render :new, locals: {
-            page: Administrate::Page::Form.new(dashboard, data_import)
-          }, status: :unprocessable_entity
-        end
+        redirect_to(
+          after_data_import_created_path(@parent, data_import),
+        )
       else
-        data_import = @source_file.data_imports.build(resource_params)
-        data_import.user = current_user
-        authorize_resource(data_import)
-
-        if data_import.save
-          ProcessDataImportJob.perform_later(data_import: data_import, last_file: last_file_flag)
-
-          redirect_to(
-            after_data_import_created_path(@source_file, data_import),
-            notice: "Thank you for submitting your occupation standard!"
-          )
-        else
-          render :new, locals: {
-            page: Administrate::Page::Form.new(dashboard, data_import)
-          }, status: :unprocessable_entity
-        end
+        render :new, locals: {
+          page: Administrate::Page::Form.new(dashboard, data_import)
+        }, status: :unprocessable_entity
       end
     end
 
     def update
       if requested_resource.update(resource_params)
         ProcessDataImportJob.perform_later(data_import: requested_resource, last_file: last_file_flag)
-        if Flipper.enabled?(:show_imports_in_administrate)
-          redirect_to(
-            after_resource_updated_path(@import, requested_resource),
-            notice: translate_with_resource("update.success")
-          )
-        else
-          redirect_to(
-            after_resource_updated_path(@source_file, requested_resource),
-            notice: translate_with_resource("update.success")
-          )
-        end
+        redirect_to(
+          after_resource_updated_path(@parent, requested_resource),
+          notice: translate_with_resource("update.success")
+        )
       else
         render :edit, locals: {
           page: Administrate::Page::Form.new(dashboard, requested_resource)
@@ -89,20 +58,16 @@ module Admin
       else
         flash[:error] = requested_resource.errors.full_messages.join("<br/>")
       end
-      if Flipper.enabled?(:show_imports_in_administrate)
-        redirect_to after_resource_destroyed_path(@import)
-      else
-        redirect_to after_resource_destroyed_path(@source_file)
-      end
+      redirect_to after_resource_destroyed_path(@parent)
     end
 
     private
 
     def set_parent
-      if Flipper.enabled?(:show_imports_in_administrate)
-        @import = Imports::Pdf.find(params[:import_id])
+      @parent = if Flipper.enabled?(:show_imports_in_administrate)
+        Imports::Pdf.find(params[:import_id])
       else
-        @source_file = SourceFile.find(params[:source_file_id])
+        SourceFile.find(params[:source_file_id])
       end
     end
 
