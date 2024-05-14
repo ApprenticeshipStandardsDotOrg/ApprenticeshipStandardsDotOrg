98abb5eb352f15a2a7e8418b76963a098f1df322
Jeanine Soterwood <jeanine@thoughtbot.com>
2024-05-14 15:25:08 -0700

    Add redacted_at field to imports table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_08_181029) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_14_222448) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -136,6 +136,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_08_181029) do
     t.datetime "updated_at", null: false
     t.datetime "processed_at", precision: nil
     t.text "processing_errors"
+    t.datetime "redacted_at", precision: nil
     t.index ["assignee_id"], name: "index_imports_on_assignee_id"
     t.index ["parent_type", "parent_id"], name: "index_imports_on_parent"
     t.index ["processed_at"], name: "index_imports_on_processed_at"
