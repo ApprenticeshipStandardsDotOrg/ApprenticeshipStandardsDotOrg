f1621569b74899a5cad10fe8ff5adb11baa5c00d
Jeanine Soterwood <jeanine@thoughtbot.com>
2024-05-14 10:16:51 -0700

    Add comment on 'none' restriction for has_many data_imports

--- app/models/import.rb
+++ app/models/import.rb
@@ -1,6 +1,10 @@
 class Import < ApplicationRecord
   belongs_to :parent, polymorphic: true
   belongs_to :assignee, class_name: "User", optional: true
+
+  # DataImport records can only be linked to type Imports::Pdf,
+  # but this association is needed for all types due to limitations
+  # of Administrate.
   has_many :data_imports, -> { none }, inverse_of: "import"
 
   enum :status, [
