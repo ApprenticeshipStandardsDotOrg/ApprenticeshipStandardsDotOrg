43157829297b65bac0af5fb3d2d596932d628c5b
Jeanine Soterwood <jeanine@thoughtbot.com>
2024-05-15 10:30:14 -0700

    Customize show page for imports::pdf records

---
+++ spec/system/admin/imports/show_spec.rb
@@ -0,0 +1,47 @@
+require "rails_helper"
+
+RSpec.describe "admin/imports/show", :admin do
+  context "when Imports::Pdf" do
+    context "when admin" do
+      it "does not have button for needing support but has data import and redact buttons" do
+        stub_feature_flag(:show_imports_in_administrate, true)
+
+        admin = create(:admin)
+        import = create(:imports_pdf)
+
+        login_as admin
+        visit admin_import_path(import)
+
+        expect(page).to have_text "Imports::Pdf"
+        expect(page).to_not have_button "Needs support"
+        expect(page).to have_link "New data import", href: new_admin_import_data_import_path(import)
+        expect(page).to have_link "Redact document", href: new_admin_import_redact_file_path(import)
+        expect(page).to have_link "Edit", href: edit_admin_import_path(import)
+        expect(page).to_not have_link "Destroy"
+
+        stub_feature_flag(:show_imports_in_administrate, false)
+      end
+    end
+
+    context "when converter" do
+      it "has button for needing support, data imports, and redact buttons" do
+        stub_feature_flag(:show_imports_in_administrate, true)
+
+        admin = create(:admin, :converter)
+        import = create(:imports_pdf)
+
+        login_as admin
+        visit admin_import_path(import)
+
+        expect(page).to have_text "Imports::Pdf"
+        expect(page).to have_button "Needs support"
+        expect(page).to have_link "New data import", href: new_admin_import_data_import_path(import)
+        expect(page).to have_link "Redact document", href: new_admin_import_redact_file_path(import)
+        expect(page).to_not have_link "Edit", href: edit_admin_import_path(import)
+        expect(page).to_not have_link "Destroy"
+
+        stub_feature_flag(:show_imports_in_administrate, false)
+      end
+    end
+  end
+end
