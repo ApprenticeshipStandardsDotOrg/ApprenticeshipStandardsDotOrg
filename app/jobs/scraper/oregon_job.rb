df6e26db94cf4bdcccc08a9184ef51804a388fd9
Jeanine Soterwood <jeanine@thoughtbot.com>
2024-06-24 14:17:59 -0700

    Change a blank space to underscore in file url
    Had file URL come in as:
    https://www.oregon.gov/boli/apprenticeship/Standards/7001 0159.0.pdf
    and adding the underscore fixed the
    URL. Adding + or %20 as a replacement
    resulted in a 404.

--- app/jobs/scraper/oregon_job.rb
+++ app/jobs/scraper/oregon_job.rb
@@ -31,7 +31,7 @@ class Scraper::OregonJob < Scraper::WatirJob
         standards_table = Nokogiri::HTML(standards.inner_html)
         standards_table.css("tr").each do |row|
           file = row.css("td > a").first
-          file_path = file["href"]
+          file_path = file["href"].tr(" ", "_")
 
           CreateImportFromUri.call(
             uri: base + file_path,
