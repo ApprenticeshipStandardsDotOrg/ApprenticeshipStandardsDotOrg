efd2fcb05fd9403a997efc5a75271948b2ef80e9
Jeanine Soterwood <jeanine@thoughtbot.com>
2024-06-26 16:51:15 -0700

    Update redact_file_policy spec to use import instead of source_file

--- spec/policies/redact_file_policy_spec.rb
+++ spec/policies/redact_file_policy_spec.rb
@@ -4,16 +4,16 @@ describe RedactFilePolicy do
   permissions :new? do
     it "grants access if user is an admin" do
       admin = build(:admin)
-      source_file = build(:source_file)
+      pdf = build(:imports_pdf)
 
-      expect(described_class).to permit(admin, source_file)
+      expect(described_class).to permit(admin, pdf)
     end
 
     it "grants access if user is a converter" do
       admin = build(:user, :converter)
-      source_file = build(:source_file)
+      pdf = build(:imports_pdf)
 
-      expect(described_class).to permit(admin, source_file)
+      expect(described_class).to permit(admin, pdf)
     end
   end
 end
