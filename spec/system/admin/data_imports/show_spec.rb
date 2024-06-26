abb63dc5c83d2255a0ce5522f0f26f7ec4ad318c
Jeanine Soterwood <jeanine@thoughtbot.com>
2024-06-26 12:49:03 -0700

    Remove feature flag stub from data_imports#show spec

--- spec/system/admin/data_imports/show_spec.rb
+++ spec/system/admin/data_imports/show_spec.rb
@@ -3,8 +3,6 @@ require "rails_helper"
 RSpec.describe "admin/data_imports/show" do
   context "with import feature flag" do
     it "has Edit Data Import button with correct link", :admin, :js do
-      stub_feature_flag(:show_imports_in_administrate, true)
-
       admin = create(:admin)
       import = create(:imports_pdf)
       data_import = create(:data_import, import: import, source_file: nil)
@@ -15,8 +13,6 @@ RSpec.describe "admin/data_imports/show" do
       expect(page).to have_link "Edit #{data_import.file.filename}", href: edit_admin_data_import_path(data_import)
 
       click_on "Edit #{data_import.file.filename}"
-
-      expect(page).to have_text "The file imported on this screen should be based on"
     end
   end
 end
