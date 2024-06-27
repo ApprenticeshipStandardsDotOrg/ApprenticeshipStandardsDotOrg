66ec774124e5a9efe51298ff971d60ac615209e0
Jeanine Soterwood <jeanine@thoughtbot.com>
2024-06-27 15:12:34 -0700

    Restore random spec ordering

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,7 +41,7 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-#  config.order = :random
+  config.order = :random
 
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
   # have no way to turn it off -- the option exists only for backwards
