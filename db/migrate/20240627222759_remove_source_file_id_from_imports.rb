eecadfcf7ffc5750343a6e352f48810dbb5d7774
Jeanine Soterwood <jeanine@thoughtbot.com>
2024-06-27 15:28:28 -0700

    Remove source_file_id from imports table

---
+++ db/migrate/20240627222759_remove_source_file_id_from_imports.rb
@@ -0,0 +1,5 @@
+class RemoveSourceFileIdFromImports < ActiveRecord::Migration[7.1]
+  def change
+    remove_reference :imports, :source_file, null: true, foreign_key: true, type: :uuid
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_27_222708) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_27_222759) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -135,11 +135,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_27_222708) do
     t.datetime "processed_at", precision: nil
     t.text "processing_errors"
     t.datetime "redacted_at", precision: nil
-    t.uuid "source_file_id"
     t.index ["assignee_id"], name: "index_imports_on_assignee_id"
     t.index ["parent_type", "parent_id"], name: "index_imports_on_parent"
     t.index ["processed_at"], name: "index_imports_on_processed_at"
-    t.index ["source_file_id"], name: "index_imports_on_source_file_id"
   end
 
   create_table "industries", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -365,7 +363,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_27_222708) do
   add_foreign_key "data_imports", "imports"
   add_foreign_key "data_imports", "occupation_standards"
   add_foreign_key "data_imports", "users"
-  add_foreign_key "imports", "source_files"
   add_foreign_key "imports", "users", column: "assignee_id"
   add_foreign_key "occupation_standards", "industries"
   add_foreign_key "occupation_standards", "occupations"
