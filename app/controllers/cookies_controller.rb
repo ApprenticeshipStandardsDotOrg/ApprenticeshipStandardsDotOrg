ab102bb7d83d7f92637860dcf747c0923d7cb1b6
Fernando Perales <fer@thoughtbot.com>
2024-09-09 12:28:26 -0500

    Redirect to root path if no referrer present

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -1,6 +1,6 @@
 class CookiesController < ApplicationController
   def create
     session[:cookies_accepted] = params[:cookies] if params[:cookies]
-    redirect_to request.referrer, format: :html
+    redirect_to (request.referrer || root_path), format: :html
   end
 end
--- spec/requests/cookies_spec.rb
+++ spec/requests/cookies_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Cookies", type: :request do
   describe "GET /create" do
     it "returns http success" do
       get "/cookies/create"
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:redirect)
     end
   end
 end
