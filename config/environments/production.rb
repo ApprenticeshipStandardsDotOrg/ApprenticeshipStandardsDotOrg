af2446d50cb0ac86c86b24a891510218dd665a64
Christopher Kuttruff <chris@workhands.us>
2026-09-15 09:48:31 -0700

    tweak to sidekiq for heroku environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,11 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :redis_cache_store, {url: ENV["REDIS_URL"]}
+  redis_url = ENV.fetch(ENV.fetch("REDIS_PROVIDER", "REDIS_URL"), nil)
+  config.cache_store = :redis_cache_store, {
+    url: redis_url,
+    ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}
+  }
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :sidekiq
